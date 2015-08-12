8c9c12536a212065cec18f427dee096a502cafd1
maxim <uzumaxy@gmail.com>
2015-08-12 12:54:18 +0300

    Fixed bug.

--- packages/stale-session/stale-session.js
+++ packages/stale-session/stale-session.js
@@ -3,7 +3,7 @@ StaleSession = new function () {
 
   self.configure = function (kwargs) {
     kwargs = kwargs || {};
-    self.inactivityTimeout = kwargs.inactivityTimeout || 60000;
+    self.inactivityTimeout = kwargs.inactivityTimeout || 600000;
     self.heartbeatInterval = kwargs.heartbeatInterval || 500;
     self.activityEvents = kwargs.activityEvents ||
       'mousemove click keydown touchstart touchend';
@@ -16,24 +16,24 @@ StaleSession = new function () {
 
   self.reset = function () {
     self.lastActivity = new Date();
-    Session.set('sessionExpiration', false);
+    Session.set('sessionExpired', false);
     self.onReset();
   };
 
   var tick = function () {
+    var isExpired = Session.get('sessionExpired');
+    if (isExpired) {
+      return;
+    }
     var interval = new Date() - self.lastActivity;
     if (interval >= self.inactivityTimeout) {
-      Session.set('sessionExpiration', true);
+      Session.set('sessionExpired', true);
       self.onSessionExpiration();
-    }
-    else {
-      Session.set('sessionExpiration', false);
     }
     Meteor.setTimeout(tick.bind(self), self.heartbeatInterval);
   };
 
   Meteor.startup(function () {
-    Session.set('sessionExpiration', false);
     $(document).on(self.activityEvents, function() {
       self.lastActivity = new Date();
     });
