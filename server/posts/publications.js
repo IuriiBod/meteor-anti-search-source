f7806f382f3e4557cf9041de88d3ad005d90137f
Nadee <nadee013@gmail.com>
2015-08-30 22:03:11 +0530

    limit removed

--- server/posts/publications.js
+++ server/posts/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish('posts', function() {
     var cursor = [];
-    cursor.push(Posts.find({}, {sort: {"createdOn": -1}, limit: 20}));
+    cursor.push(Posts.find({}, {sort: {"createdOn": -1}}));
     logger.info("Posts published");
     return cursor;
 });
