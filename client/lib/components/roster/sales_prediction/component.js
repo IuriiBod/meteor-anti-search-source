82e7b7f64890b3ab136a4aed1e2573f0ef37219c
Taras Dolynyuk <trsdln@gmail.com>
2015-09-09 12:47:40 +0300

    Finished mockup for forecast view

--- client/lib/components/roster/sales_prediction/component.js
+++ client/lib/components/roster/sales_prediction/component.js
@@ -1,3 +1,28 @@
 var component = FlowComponents.define("salesPrediction", function (props) {
+  var currentWeekDate = {
+    year: Router.current().params.year,
+    week: parseInt(Router.current().params.week)
+  };
+  this.set('currentWeekDate', currentWeekDate);
 });
+
+
+component.state.week = function () {
+  var currentWeekDate = this.get('currentWeekDate');
+  return getDatesFromWeekNumberWithYear(currentWeekDate.week, new Date(currentWeekDate.year));
+};
+
+
+//MOCK DATA
+
+component.state.menuItems = function () {
+  return [
+    'Brisket Special', 'Caramel Banana bread', 'Cupcakes Platter', 'Eton Mess', 'Fudge Brownie - Vegan', 'Muesli'
+  ];
+};
+
+component.state.random = function (zeros) {
+  return Math.floor(Math.random() * Math.pow(10, zeros));
+};
+
 
--- client/lib/components/roster/sales_prediction/view.js
+++ client/lib/components/roster/sales_prediction/view.js
@@ -1,17 +1,2 @@
-var random = function (zeros) {
-  return Math.floor(Math.random() * Math.pow(10, zeros));
-};
 
-var menuItems = [
-  'Brisket Special', 'Caramel Banana bread', 'Cupcakes Platter', 'Eton Mess', 'Fudge Brownie - Vegan', 'Muesli'
-].map(function (itemName) {
-    return {name: itemName, predictedSellCount: random(2)};
-  });
-
-
-Template.salesPrediction.helpers({
-  salesPerMenuItem: function () {
-    return menuItems;
-  }
-});
 
