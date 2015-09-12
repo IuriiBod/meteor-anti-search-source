74c0d0588b1a1ef2f4e394282363c1455bd02d5c
Nadee <nadee013@gmail.com>
2015-09-12 23:20:24 +0530

    fixed ordering shifts

--- client/lib/components/roster/weekly/bottom/day/component.js
+++ client/lib/components/roster/weekly/bottom/day/component.js
@@ -1,6 +1,7 @@
 var component = FlowComponents.define("weeklyRosterDay", function(props) {
   this.name = props.name;
   this.origin = props.origin;
+  this.onRendered(this.onListRendered); 
 });
 
 component.state.name = function() {
@@ -25,7 +26,7 @@ component.state.shifts = function() {
   if(origin == "weeklyroster") {
     var week = Session.get("thisWeek");
     var date = this.name.date;
-    return Shifts.find({"shiftDate": new Date(date).getTime(), "type": null});
+    return Shifts.find({"shiftDate": new Date(date).getTime(), "type": null}, {sort: {"order": 1}});
   } else if(origin == "weeklyrostertemplate") {
     var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     return Shifts.find({"shiftDate": daysOfWeek.indexOf(this.name), "type": "template"});
@@ -64,5 +65,86 @@ component.state.isTemplate = function() {
     return true; 
   } else {
     return false;
+  }
+}
+
+component.prototype.onListRendered = function() {
+  var user = Meteor.user();
+  $(".col-lg-13:first").css("margin-left", "0px");
+  if(user.isAdmin || Meteor.isManager) {
+    $(".sortable-list > div > li").css("cursor", "move");
+    var origin = this.name;
+    $(".sortable-list").sortable({
+      "connectWith": ".sortable-list",
+      "revert": true
+    });
+
+    $(".sortable-list").on("sortstop", function(event, ui) {
+      var id = $(ui.item[0]).find("li").attr("data-id");//shiftid
+
+      var order = 0;
+      var nextOrder = $(ui.item[0]).next().find("li").attr("data-order");
+      var prevOrder = $(ui.item[0]).prev().find("li").attr("data-order");
+
+      var thisShift = Shifts.findOne(id);
+
+      if(thisShift) {
+        if(!nextOrder) {
+          order = parseFloat(prevOrder) + 1;
+        } else if(!prevOrder) {
+          order =  parseFloat(nextOrder) - 1;
+        } else {
+          order = (parseFloat(nextOrder) + parseFloat(prevOrder))/2;
+        }
+      }
+
+      Meteor.call("editShift", id, {"order": order}, function(err) {
+        if(err) {
+          console.log(err);
+          $(ui.sender[0]).sortable('cancel');;
+          return alert(err.reason);
+        }
+      });
+    });
+
+
+    $(".sortable-list").on("sortreceive", function(event, ui) {
+
+      var self = this;
+      var id = $(ui.item[0]).find("li").attr("data-id");//shiftid
+      var newDate = $(this).attr("data-date")//date of moved list
+      if(origin == "weeklyrostertemplate") {
+        var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        newDate = parseInt(daysOfWeek.indexOf(newDate));
+      }
+
+      var order = 0;
+      var nextOrder = $(ui.item[0]).next().find("li").attr("data-order");
+      var prevOrder = $(ui.item[0]).prev().find("li").attr("data-order");
+
+      var thisShift = Shifts.findOne(id);
+
+      if(thisShift) {
+        if(!nextOrder) {
+          order = parseFloat(prevOrder) + 1;
+        } else if(!prevOrder) {
+          order =  parseFloat(nextOrder) - 1;
+        } else {
+          order = (parseFloat(nextOrder) + parseFloat(prevOrder))/2;
+        }
+      }
+
+      if(id && newDate) {
+        Meteor.call("editShift", id, {"shiftDate": newDate, "order": order}, function(err) {
+          if(err) {
+            console.log(err);
+            $(ui.sender[0]).sortable('cancel');;
+            return alert(err.reason);
+          }
+        });
+      }
+    });
+  } else {
+    $(".sortable-list > div > li").css("cursor", "default");
   }
 }
--- server/shifts/index.js
+++ server/shifts/index.js
@@ -39,6 +39,13 @@ Meteor.methods({
       type = info.type;
     }
 
+    var order = 0;
+    var shifts = Shifts.find({"shiftDate": new Date(info.shiftDate).getTime()}).fetch();
+    if(shifts) {
+      order = shifts.length;
+    }
+
+
     var doc = {
       "startTime": new Date(info.startTime).getTime(),
       "endTime": new Date(info.endTime).getTime(),
@@ -50,7 +57,8 @@ Meteor.methods({
       "jobs": [],
       "status": "draft",
       "type": type,
-      "published": false
+      "published": false,
+      "order": order
     }
     if(info.hasOwnProperty("week") && info.week.length > 0) {
       var alreadyPublished = Shifts.findOne({"shiftDate": {$in: info.week}, "published": true});
@@ -170,6 +178,11 @@ Meteor.methods({
         updateDoc.shiftDate = new Date(info.shiftDate).getTime();
       }
     }
+
+    if(info.order) {
+      updateDoc['order'] = parseFloat(info.order);
+    }
+
     if(info.assignedTo) {
       var date = null;
       if(updateDoc.shiftDate) {
