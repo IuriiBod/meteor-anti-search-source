7a27d96e04b8e1f59d573ce567f105f91568fbe0
Nadee <nadee013@gmail.com>
2015-09-15 09:59:32 +0530

    fixed progress issue

--- client/lib/components/stocktake/counting/areas/area_box/component.js
+++ client/lib/components/stocktake/counting/areas/area_box/component.js
@@ -22,7 +22,15 @@ component.state.widthofBar = function() {
     var sProgress = 0;
     var specialArea = SpecialAreas.findOne(id);
     if(specialArea && specialArea.stocks) {
-      if(specialArea.stocks.length > 0 && stocktakes.length > 0) {
+      if(specialArea.stocks.length > 0) {
+        var stocktakes = Stocktakes.find({
+          $and: [
+            {"stockId": {$in: specialArea.stocks}},
+            {"version": Session.get("thisVersion")},
+            {"specialArea": id},
+            {'generalArea': Session.get("activeGArea")}        
+          ]
+        }).fetch();
         var stocks = Ingredients.find({"_id": {$in: specialArea.stocks}, "status": "active"}).fetch();
         if(stocks && stocks.length > 0) {
           sProgress = (stocktakes.length/stocks.length) * 100;
