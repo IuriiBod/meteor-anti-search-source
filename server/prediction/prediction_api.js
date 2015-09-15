3d18f08bfef53e375d9722de0543f5dae3b5ba26
brmk <paulbrmk@gmail.com>
2015-09-15 11:30:06 +0300

    configured path for prediction package and done auth

--- server/prediction/prediction_api.js
+++ server/prediction/prediction_api.js
@@ -1,11 +1,12 @@
 Meteor.methods({
   "getAuthToken": function () {
     var Client = Meteor.npmRequire('node-google-prediction');
+    var path = process.env.PWD;
     var client = new Client({claimSetISS: "135160182081-plkk2djriqo90v2l41461a9b03qb0tmt@developer.gserviceaccount.com",
-                             path: "/home/brmk/Projects/herochef/private/hero-chef-secret.pem"
+                             path: path + "/private/hero-chef-secret.pem"
                             });
 
-    var syncToken = Meteor.wrapAsync(client.accessTokenRequest);
+    var syncToken = Meteor.wrapAsync(client.accessTokenRequest, client);
     return syncToken()
   }
 });
