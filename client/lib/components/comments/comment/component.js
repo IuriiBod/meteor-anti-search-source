804a2ec8655e166f99fe188b377774a0aeb92911
Nadee <nadee013@gmail.com>
2015-09-16 11:55:25 +0530

    fix uploaded profile pic at comments

--- client/lib/components/comments/comment/component.js
+++ client/lib/components/comments/comment/component.js
@@ -1,18 +1,6 @@
 var component = FlowComponents.define("comment", function(props) {
   this.comment = props.comment;
-  this.userDetails();
 });
-
-component.prototype.userDetails = function() {
-  var user = Meteor.users.findOne(this.comment.createdBy);
-  if(user) {
-    var image = "/images/user-image.jpeg";
-    if(user.services && user.services.google) {
-      image = user.services.google.picture;
-    }
-    this.set("profileImage", image);
-  }
-};
 
 component.state.createdBy = function() {
   return this.comment.createdBy;
