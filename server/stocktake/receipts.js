13b542923292812a9fd981c99b8b5b97cac3517e
Nadee <nadee013@gmail.com>
2015-09-17 16:46:19 +0530

    Merge branch 'supplier-list-upload' into smaill-bug-fixes

--- server/stocktake/receipts.js
+++ server/stocktake/receipts.js
@@ -200,5 +200,26 @@ Meteor.methods({
         throw new Meteor.Error("Receipt does not exist");
       }
     }
+  },
+
+  uploadInvoice: function(id, info) {
+    if(!Meteor.userId()) {
+      logger.error('No user has logged in');
+      throw new Meteor.Error(401, "User not logged in");
+    }
+    var userId = Meteor.userId();
+    var permitted = isManagerOrAdmin(userId);
+    if(!permitted) {
+      logger.error("User not permitted to generate receipts");
+      throw new Meteor.Error(404, "User not permitted to generate receipts");
+    }
+
+    var receipt = OrderReceipts.findOne(id);
+    if(!receipt) {
+      logger.error('Receipt not found');
+      throw new Meteor.Error(404, "Receipt not found");
+    }
+    OrderReceipts.update({"_id": id}, {$addToSet: {"invoiceImage": info}});
+    logger.info("Invoice uploaded", id);
   }
 });
