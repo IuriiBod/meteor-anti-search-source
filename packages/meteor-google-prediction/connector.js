f1ced2305938821565ff72dca8e262e0243b3d7d
Taras Dolynyuk <trsdln@gmail.com>
2015-09-22 14:26:03 +0300

    Created Google prediction client package

---
+++ packages/meteor-google-prediction/connector.js
@@ -0,0 +1,65 @@
+var PredictionClient = Npm.require('node-google-prediction');
+
+
+GooglePrediction = function GooglePrediction(options) {
+  this._authOptions = {
+    claimSetISS: options.serviceEmail,
+    //pem should be located in private directory
+    path: this._assetsFolderAbsolutePath(options.pemFile)
+  };
+  this.projectName = options.projectName;
+};
+
+
+GooglePrediction.prototype.list = function () {
+  return this._makeRequest(this._baseUrl('list'));
+};
+
+
+GooglePrediction.prototype._auth = function () {
+  this._client = new PredictionClient(this._authOptions);
+  var syncToken = Meteor.wrapAsync(this._client.accessTokenRequest, this._client);
+  return syncToken();
+};
+
+
+GooglePrediction.prototype._makeRequest = function (url, data) {
+  var authCredentials = this._auth();
+  try {
+    var result = HTTP.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": authCredentials.token_type + ' ' + authCredentials.access_token
+      },
+      data: data
+    });
+    return result.data;
+  } catch (err) {
+    throw new Meteor.Error(500, 'Error while access Prediction API: ' + err.reason + '\nURL: ' + url)
+  }
+};
+
+
+GooglePrediction.prototype._baseUrl = function (method, modelId) {
+  var suffix = method;
+  if (modelId) {
+    suffix = modelId + '/' + suffix;
+  }
+  return 'https://www.googleapis.com/prediction/v1.6/projects/' + this.projectName + '/trainedmodels/' + suffix;
+};
+
+
+GooglePrediction.prototype._assetsFolderAbsolutePath = function (fileName) {
+  var fs = Npm.require('fs');
+  var path = Npm.require('path');
+
+  var meteorRoot = fs.realpathSync(process.cwd() + '/../');
+
+  var assetsFolder = meteorRoot + '/server/assets/app';
+
+  if (fileName) {
+    assetsFolder += '/' + fileName
+  }
+
+  return assetsFolder
+};
--- server/prediction/prediction-api/connector.js
+++ server/prediction/prediction-api/connector.js
@@ -1,29 +1,17 @@
 var CloudSettings = Meteor.settings.private.GoogleCloud;
-var PredictionClient = Meteor.npmRequire('node-google-prediction');
 
 PredictionApi = {
-  auth: function () {
-    this._client = new PredictionClient({
-      claimSetISS: CloudSettings.SERVICE_EMAIL,
-      //pem should be located in private directory
-      path: this._assetsFolderAbsolutePath(CloudSettings.PEM_FILE)
-    });
-    var syncToken = Meteor.wrapAsync(this._client.accessTokenRequest, this._client);
-    return syncToken()
-  },
+  test: function () {
+    var authOptions = {
+      serviceEmail: CloudSettings.SERVICE_EMAIL,
+      pemFile: CloudSettings.PEM_FILE,
+      projectName: 'HospoHero'
+    };
 
-  _assetsFolderAbsolutePath: function (fileName) {
-    var fs = Npm.require('fs');
-    var path = Npm.require('path');
-
-    var meteorRoot = fs.realpathSync(process.cwd() + '/../');
-
-    var assetsFolder = meteorRoot + '/server/assets/app';
-
-    if (fileName) {
-      assetsFolder += '/' + fileName
-    }
-
-    return assetsFolder
+    var googlePrediction = new GooglePrediction(authOptions);
+    console.log(googlePrediction.list())
   }
 };
+
+
+
