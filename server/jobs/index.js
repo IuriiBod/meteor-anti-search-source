058177bdc76a694a1f00f50b8db7cf8c2a4976ba
Nadee <nadee013@gmail.com>
2015-09-22 16:28:16 +0530

    fix time error in recurring

--- server/jobs/index.js
+++ server/jobs/index.js
@@ -51,7 +51,7 @@ Meteor.methods({
         throw new Meteor.Error(404, "Active time not valid");
       }
     } else {
-      doc.activeTime = parseInt(info.activeTime);
+      doc.activeTime = parseInt(info.activeTime) * 60;
       doc.section = job.section;
       doc.startAt = job.repeatAt;
     }
