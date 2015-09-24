9137a6e8943d621d1b2dd2f5d1c257f8b759c5ca
YuraSrohiy <yuranesh@mail.ru>
2015-09-24 17:32:40 +0300

    added notification for predictions

--- server/prediction/shedule-update/cron_update.js
+++ server/prediction/shedule-update/cron_update.js
@@ -2,14 +2,14 @@ Meteor.startup(function(){
   SyncedCron.add({
     name: 'Forecast refresh',
     schedule: function(parser) {
-    return parser.text('at 11:31 am');
+    return parser.text('at 05:00 am');
     },
     job: function() {
       var date = moment();
 
       if(!ForecastDates.findOne({locationId: 1})) {
         ForecastDates.insert({locationId: 1, lastThree: date.format(), lastSixWeeks: date.format()});
-        predict(31);
+        predict(84);
 
       }
       else {
@@ -17,8 +17,6 @@ Meteor.startup(function(){
 
         if (date.diff(lastUpdates.lastSixWeeks) >= 1000*60*60*24*42) {
           predict(84);
-          console.log(date.diff(lastUpdates.lastSixWeeks));
-          console.log("6 weeks prediction");
           ForecastDates.update({locationId: 1}, {$set:{lastSixWeeks: date.format(), lastThree: date.format() }});
         }else if(date.diff(lastUpdates.lastThree) >= 1000*60*60*24*3) {
           predict(7);
@@ -39,6 +37,7 @@ function predict (days){
   var updateDay = moment().format();
   var prediction = PredictionApi.auth();
   var Items = MenuItems.find({},{fields:{_id: 1}}).fetch();
+  var notificationText = [];
 
   for(var i = 1; i<=days; i++ ) {
     var weather = OpenWeatherMap.historyMock();                             //here will be weather for day we need
@@ -47,15 +46,42 @@ function predict (days){
 
     _.each(Items, function(item){                                        
       var dataVector = [item._id, weather.temp, weather.main, dayOfYear];
-      var quantity = prediction.predict("trainingModel", dataVector);
+      var quantity = parseInt(prediction.predict("trainingModel", dataVector).outputValue);
 
-      var predictItem ={
+      var predictItem = {
         date: moment(updateDay).format("YYYY-MM-DD"),
-        quantity: parseInt(quantity.outputValue),
+        quantity: quantity,
         updateAt: date,
         menuItemId: item._id
       };
-      SalesPrediction.insert(predictItem);
+
+      var currentData = SalesPrediction.findOne({date: predictItem.date, menuItemId: predictItem.menuItemId});
+      if (i<14 && currentData){
+        if (currentData.quantity != predictItem.quantity)
+        {
+          var itemName = MenuItems.findOne({_id: predictItem.menuItemId}).name;
+          notificationText.push("<li>" + predictItem.date + ":" + itemName + ": from " + currentData.quantity + " to " + predictItem.quantity + "</li>");
+        }
+      }
+      SalesPrediction.update({date: predictItem.date, menuItemId: predictItem.menuItemId},predictItem, {upsert:true});
     });
+  }
+  if (notificationText.length != 0)
+  {
+    notificationText.push("</ul>");
+    notificationText.unshift("<ul>");
+    var receivers = Meteor.users.find({isManager: true}).fetch();
+    var options = {
+      type: 'prediction',
+      read: false,
+      title: 'Some predictions have been changed',
+      createdBy: null,
+      text: notificationText.join(''),
+      actionType: 'update'
+    };
+    for (i=0; i<receivers.length; i++){
+      options.to = receivers[i]._id;
+      Notifications.insert(options);
+    }
   }
 }
