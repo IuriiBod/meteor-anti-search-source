7457312b6db2f8d3a7f38c51105b2da3bad0e3c2
alonoslav <vadym.vorobel@gmail.com>
2015-09-28 12:06:32 +0300

    Changed stock list

--- client/lib/components/stock/master_list/item/component.js
+++ client/lib/components/stock/master_list/item/component.js
@@ -6,19 +6,6 @@ component.state.item = function() {
   return this.ingredient;
 };
 
-component.state.isManagerOrAdmin = function() {
-  var user = Meteor.user();
-  if(user && (user.isManager || user.isAdmin)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 component.state.isArchive = function() {
-  if(this.ingredient.status == 'archived') {
-    return true;
-  } else {
-    return false;
-  }
-}
+  return this.ingredient.status == 'archived';
+};
