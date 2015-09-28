32c574e90dd2c78ae2789d7c6bf8b5e9ce67712d
Nadee <nadee013@gmail.com>
2015-09-28 16:36:36 +0530

    fixed bug

--- client/lib/components/menuitems/settings/component.js
+++ client/lib/components/menuitems/settings/component.js
@@ -1,25 +1,6 @@
-var subs = new SubsManager();
-
 var component = FlowComponents.define('settingsMenuItem', function(props) {
-  this.id = Router.current().params._id;
-  this.onRendered(this.onMenuRendered);
+  this.id = Session.get("thisMenuItem");
 });
-
-component.state.initialHTML = function() {
-  var id = Session.get("thisMenuItem");
-  var item = MenuItems.findOne(id);
-  if(item) {
-    if(item.instructions) {
-      return item.instructions;
-    } else {
-      return "Add instructions here"
-    }
-  }
-};
-
-component.prototype.onMenuRendered = function() {
-  this.item = MenuItems.findOne(this.id);
-}
 
 component.state.menu = function() {
   this.item = MenuItems.findOne(this.id);
@@ -30,27 +11,33 @@ component.state.menu = function() {
 
 component.state.myCategory = function(categoryId) {
   this.item = MenuItems.findOne(this.id);
-  var myCategory = this.item.category;
-  if(myCategory) {
-    return Categories.findOne(myCategory);
+  if(this.item) {
+    var myCategory = this.item.category;
+    if(myCategory) {
+      return Categories.findOne(myCategory);
+    }
   }
 }
 
 
 component.state.categoriesList = function() {
-  var myCategory = this.item.category;
-  if(myCategory) {
-    return Categories.find().fetch();
+  if(this.item) {
+    var myCategory = this.item.category;
+    if(myCategory) {
+      return Categories.find().fetch();
+    }
   }
 }
 
 component.state.statusList = function() {
-  var myStatus = this.item.status;
-  var list = null;
-  if(myStatus) {
-    list = Statuses.find().fetch();
+  if(this.item) {
+    var myStatus = this.item.status;
+    var list = null;
+    if(myStatus) {
+      list = Statuses.find().fetch();
+    }
+    return list;
   }
-  return list;
 }
 
 component.state.isArchived = function() {
