3666c1ff555683d5555b5895215e815c6a6834a2
Taras Dolynyuk <trsdln@gmail.com>
2015-09-29 15:43:58 +0300

    Refactored training data update task

--- server/prediction/scheduling/training-data.js
+++ server/prediction/scheduling/training-data.js
@@ -1,4 +1,31 @@
-currentLocationId = 1;
+var currentLocationId = 1;
+
+var updateLastTaskRunDateForLocation = function (locationId) {
+  //update task run date
+  ForecastDates.update({locationId: locationId}, {
+    $set: {
+      lastUploadDate: new Date(),
+      locationId: locationId
+    }
+  }, {upsert: true});
+};
+
+var createUpdateActualSalesFunction = function () {
+  //updates sales data for previous day
+  var isHandledFirstDay = false;
+  var previousDayMoment = moment().subtract(1, 'day');
+
+  return function (salesData) {
+    if (!isHandledFirstDay && previousDayMoment.isSame(salesData.createdDate, 'day')) {
+      //todo save sales data here
+      console.log('data to update ', salesData);
+      isHandledFirstDay = false;
+      return false;
+    }
+    return true;
+  };
+};
+
 
 SyncedCron.add({
   name: 'Prediction model refresh',
@@ -6,22 +33,38 @@ SyncedCron.add({
     return parser.text('at 03:00 am');
   },
   job: function () {
-    var predictionApi = new GooglePredictionApi();
+    //todo: update it for all locations
     var forecastData = ForecastDates.findOne({locationId: currentLocationId});
-    if (!forecastData){
-      ForecastDates.insert({locationId: currentLocationId, lastUploadDate: moment().toDate()});
-      predictionApi.updatePredictionModel();
-    }else if(!forecastData.lastUploadDate || forecastData.lastUploadDate >= getMillisecondsFromDays(182)){
-      ForecastDates.update({locationId: currentLocationId}, {$set:{lastUploadDate: moment().toDate()}});
-      predictionApi.updatePredictionModel();
+
+    var needToUpdateModel = !forecastData || !forecastData.lastUploadDate || forecastData.lastUploadDate >= getMillisecondsFromDays(182);
+
+    var updateActualSalesFn = createUpdateActualSalesFunction()
+
+    if (needToUpdateModel) {
+      if (!HospoHero.isDevelopmentMode()) {
+        //todo: update it for organizations
+        var predictionApi = new GooglePredictionApi();
+        var updateSession = predictionApi.getUpdatePredictionModelSession();
+
+        //upload sales training data for the last year
+        Revel.uploadAndReduceOrderItems(function (salesData) {
+          updateActualSalesFn(salesData);
+          return updateSession.onDataReceived(salesData);
+        });
+
+        updateSession.onUploadingFinished();
+
+        updateLastTaskRunDateForLocation(currentLocationId)
+      }
+
+
+    } else {
+      //update sales for last day only
+      Revel.uploadAndReduceOrderItems(function (salesData) {
+        return updateActualSalesFn(salesData);
+      });
     }
-
-
-
-    //if (!HospoHero.isDevelopmentMode()) {
-    //  //todo: update it for organizations
-    //  var predictionApi = new GooglePredictionApi();
-    //  predictionApi.updatePredictionModel();
-    //}
   }
 });
+
+
