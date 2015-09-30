a2448bfbec04d05f361389eb26b68dc4899e29ff
Taras Dolynyuk <trsdln@gmail.com>
2015-09-30 19:42:34 +0300

    Refactored sales forecast component

--- client/lib/components/sales_prediction/component.js
+++ client/lib/components/sales_prediction/component.js
@@ -7,33 +7,6 @@ component.state.week = function () {
   return HospoHero.dateUtils.getWeekDays(currentWeekDate);
 };
 
-component.state.weekPrediction = function (id) {
-  var currentWeekDate = this.get('currentWeekDate');
-  var monday = moment(HospoHero.dateUtils.getDateByWeekDate(currentWeekDate));
-
-  var dates = _.map(HospoHero.dateUtils.getWeekDays(currentWeekDate), function (date) {
-    return moment(date).format('YYYY-MM-DD');
-  });
-  
-  var prediction = _.map(SalesPrediction.find({
-    date: TimeRangeQueryBuilder.forWeek(monday),
-    menuItemId: id
-  }, {sort: {date: 1}}).fetch(), function (item) {
-    return {date: moment(item.date).format('YYYY-MM-DD'), predictionQuantity: item.quantity};
-  });
-  var actual = _.map(ImportedActualSales.find({
-    date: TimeRangeQueryBuilder.forWeek(monday),
-    menuItemId: id
-  }, {sort: {date: 1}}).fetch(), function (item) {
-    return {date: moment(item.date).format('YYYY-MM-DD'), actualQuantity: item.quantity};
-  });
-
-  prediction = _.sortBy(importMissingData(dates, prediction, "predictionQuantity"), "date");
-  actual = _.sortBy(importMissingData(dates, actual, "actualQuantity"), "date");
-  var result = mergeArrays(prediction, actual);
-  return result;
-};
-
 component.state.menuItems = function () {
   return MenuItems.find().fetch();
 };
@@ -44,33 +17,6 @@ component.state.getTotalSales = function (date) {
   var actualTotal = getTotalPrice(actual);
   var predictionTotal = getTotalPrice(predictions);
   return {predicted: predictionTotal, actual: actualTotal};
-};
-
-var importMissingData = function (dates, importArray, keyName) {
-  _.each(dates, function (dateItem) {
-    var push = true;
-    _.each(importArray, function (item) {
-      if (dateItem === item.date) {
-        push = false;
-      }
-    });
-
-    if (push) {
-      var toPush = {
-        date: dateItem
-      };
-      toPush[keyName] = 0;
-      importArray.push(toPush);
-    }
-  });
-  return importArray
-};
-
-var mergeArrays = function (array1, array2) {
-  for (var i = 0; i < array1.length; i++) {
-    _.extend(array1[i], array2[i])
-  }
-  return array1
 };
 
 var getTotalPrice = function (array) {
