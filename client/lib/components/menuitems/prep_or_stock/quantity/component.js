124cd721698459f25d756a1378e3b9bb8315243b
Nadee <nadee013@gmail.com>
2015-10-01 09:30:03 +0530

    bug in prep and stock x-edit to next focus on enter

--- client/lib/components/menuitems/prep_or_stock/quantity/component.js
+++ client/lib/components/menuitems/prep_or_stock/quantity/component.js
@@ -9,26 +9,52 @@ component.state.item = function() {
   return this;
 };
 
+component.state.quantity = function() {
+  return this.quantity;
+};
+
 
 component.prototype.onItemRendered = function() {
   var menu = Session.get("thisMenuItem");
   $('.quantity').editable({
     mode: "inline",
     type: "text",
+    title: 'Edit count',
+    autotext: 'auto',
+    display: false,
+    inputclass: "nadee",
     success: function(response, newValue) {
+      var elem = $(this).closest("tr");
+      if($(elem).next().length > 0) {
+        $(elem).next().find("a.quantity").click();
+      }
       if(newValue) {
         var ing = $(this).data("pk");
         var type = $(this).data("itemtype");
+        newValue = parseFloat(newValue);
+        if(newValue == newValue) {
+          newValue = newValue;
+        } else {
+          newValue = 0;
+        }
         if(type == "ings") {
           Meteor.call("addMenuIngredients", menu, [{"_id": ing, "quantity": newValue}], function(err) {
             if(err) {
               HospoHero.alert(err);
+            } else {
+              if($(elem).next().length > 0) {
+                $(elem).next().find("a.quantity").click();
+              }
             }
           });
         } else if(type == "prep") {
           Meteor.call("addMenuPrepItems", menu, [{"_id": ing, "quantity": newValue}], function(err) {
             if(err) {
               HospoHero.alert(err);
+            } else {
+              if($(elem).next().length > 0) {
+                $(elem).next().find("a.quantity").click();
+              }
             }
           });
         }
