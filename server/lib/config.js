abddb499ea3f3687c161c694e900b8bfadae8342
Taras Dolynyuk <trsdln@gmail.com>
2015-10-02 15:13:03 +0300

    Removed old sales forecast views and server side stuff

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,6 +1,11 @@
-if(process.env.ROOT_URL == 'http://herochef-test.meteor.com') {
+MeteorSettings.setDefaults({
+  "developmentServers": [
+    "http://herochef-test.meteor.com"
+  ]
+});
+
+//find out we are on development server
+if (Meteor.settings.developmentServers.indexOf(process.env.ROOT_URL) > -1) {
   process.env.NODE_ENV = "development";
   delete process.env.MAIL_URL;
 }
-
-process.env.MAIL_URL = 'smtp://postmaster%40herochef.com.au:Bertaroo724@smtp.mailgun.org:587/';
