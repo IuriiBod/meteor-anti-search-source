2a9d27e978aaf72cf9602767651a852df348c047
alonoslav <vadym.vorobel@gmail.com>
2015-10-02 17:52:51 +0300

    Add methods to select users by permissions

--- server/lib/helpers.js
+++
@@ -1,6 +0,0 @@
-Namespace('HospoHero', {
-  currentArea: function(userId) {
-    var user = Meteor.users.findOne({_id: userId});
-    return user && user.currentAreaId ? user.currentAreaId : null;
-  }
-});
