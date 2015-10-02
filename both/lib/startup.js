782455e952060b9630171148cdf827acb52cb71a
alonoslav <vadym.vorobel@gmail.com>
2015-10-02 14:56:42 +0300

    Database migration to v.2

--- both/lib/startup.js
+++ both/lib/startup.js
@@ -3,6 +3,8 @@ if(Meteor.isServer) {
     console.log('Database version: ', Migrations.getVersion());
     if(Migrations.getVersion() == 0) {
       Migrations.migrateTo(1);
+    } else if(Migrations.getVersion() == 1) {
+      Migrations.migrateTo(2);
     }
   });
 }
---
+++ server/_migrations/v2.js
@@ -0,0 +1,47 @@
+Migrations.add({
+  version: 2,
+  name: "Change Admin to Owner",
+  up: function() {
+    var role = Meteor.roles.findOne({name: 'Admin'});
+    if(role) {
+      Meteor.roles.update({
+        _id: role._id
+      }, {
+        name: 'Owner'
+      });
+
+      role = Meteor.roles.findOne({name: 'Manager'});
+      var newPermissions = [
+        'JOB_VIEW',
+        'JOB_EDIT',
+        'MENU_VIEW',
+        'MENU_EDIT',
+        'ROSTER_VIEW',
+        'ROSTER_EDIT',
+        'ROSTER_CAN_BE_ROSTED',
+        'ROSTER_APPROVER',
+        'STOCK_VIEW',
+        'STOCK_EDIT',
+        'USER_INVITE',
+        'USER_EDIT',
+        'USER_EDIT_PAYRATE',
+        'FORECAST_VIEW',
+        'AREA_EDIT',
+        'AREA_VIEW_FINANCIAL_INFO',
+        'LOCATION_EDIT',
+        'ORGANIZATION_BILLING',
+        'ORGANIZATION_EDIT',
+        'SITE_ALL_RIGHTS'
+      ];
+
+      Meteor.roles.update({
+        _id: role._id
+      }, {
+        $set: {
+          permissions: newPermissions
+        }
+      });
+      console.log('Migration successfully completed');
+    }
+  }
+});
