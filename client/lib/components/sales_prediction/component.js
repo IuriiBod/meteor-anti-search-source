a69ce062b3c87e2b04405a159c8d16c71f8110f6
YuraSrohiy <yuranesh@mail.ru>
2015-10-05 12:09:12 +0300

    created totalSalesRow and replaced sales row in roster and forecast with it

--- client/lib/components/sales_prediction/component.js
+++ client/lib/components/sales_prediction/component.js
@@ -10,21 +10,3 @@ component.state.week = function () {
 component.state.menuItems = function () {
   return MenuItems.find().fetch();
 };
-
-component.state.getTotalSales = function (date) {
-  var predictions = SalesPrediction.find({date: TimeRangeQueryBuilder.forDay(date)}).fetch();
-  var actual = ImportedActualSales.find({date: TimeRangeQueryBuilder.forDay(date)}).fetch();
-  var actualTotal = getTotalPrice(actual);
-  var predictionTotal = getTotalPrice(predictions);
-  return {predicted: predictionTotal, actual: actualTotal};
-};
-
-var getTotalPrice = function (array) {
-  var total = 0;
-  _.each(array, function (item) {
-    var quantity = item.quantity;
-    var price = MenuItems.findOne({_id: item.menuItemId}).salesPrice;
-    total += quantity * price;
-  });
-  return total;
-};
---
+++ client/lib/components/total_sales_row/component.js
@@ -0,0 +1,29 @@
+var component = FlowComponents.define('totalSalesRow', function (props) {
+    var year = Router.current().params.year;
+    var week = Router.current().params.week;
+    this.set('currentWeekDate', {year: year, week: week});
+});
+
+
+component.state.week = function () {
+    var currentWeekDate = this.get('currentWeekDate');
+    return HospoHero.dateUtils.getWeekDays(currentWeekDate);
+};
+
+component.state.getTotalSales = function (date) {
+    var predictions = SalesPrediction.find({date: TimeRangeQueryBuilder.forDay(date)}).fetch();
+    var actual = ImportedActualSales.find({date: TimeRangeQueryBuilder.forDay(date)}).fetch();
+    var actualTotal = getTotalPrice(actual);
+    var predictionTotal = getTotalPrice(predictions);
+    return {predicted: predictionTotal, actual: actualTotal};
+};
+
+var getTotalPrice = function (array) {
+    var total = 0;
+    _.each(array, function (item) {
+        var quantity = item.quantity;
+        var price = MenuItems.findOne({_id: item.menuItemId}).salesPrice;
+        total += quantity * price;
+    });
+    return total;
+};
