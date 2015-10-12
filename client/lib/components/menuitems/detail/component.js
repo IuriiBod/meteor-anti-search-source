14c2d523884adf24032fce92f58fdd3e5485755e
alonoslav <vadym.vorobel@gmail.com>
2015-10-12 16:39:49 +0300

    Back to reactivity

--- client/lib/components/menuitems/detail/component.js
+++ client/lib/components/menuitems/detail/component.js
@@ -1,29 +1,5 @@
 var component = FlowComponents.define('menuItemDetail', function(props) {
-  LocalMenuIngsAndPreps.remove({});
-  var id = Session.get("thisMenuItem");
-  this.id = id;
-  Meteor.subscribe("menuItem", this.id);
-  var menu = MenuItems.findOne(this.id);
-  if(menu) {
-    var prepIds = [];
-    if(menu.jobItems.length > 0) {
-      menu.jobItems.forEach(function(prep) {
-        var doc = prep;
-        doc['type'] = "prep";
-        doc['menu'] = id;
-        LocalMenuIngsAndPreps.insert(doc);
-      });
-    }
-    if(menu.ingredients.length > 0) {
-      menu.ingredients.forEach(function(ings) {
-        var doc = ings;
-        doc['type'] = "ings";
-        doc['menu'] = id;
-        LocalMenuIngsAndPreps.insert(doc);
-      });
-    }
-  }
-  this.onRendered(this.onViewRendered);
+  this.id = Session.get("thisMenuItem");
 });
 
 component.state.menu = function() {
@@ -34,26 +10,25 @@ component.state.menu = function() {
 };
 
 component.state.jobItems = function() {
-  return LocalMenuIngsAndPreps.find({"type": "prep", "menu": this.id});
+  if(this.get('menu') && this.get('menu').jobItems) {
+    var jobItems = this.get('menu').jobItems;
+    jobItems = _.map(jobItems, function (jobItem) {
+      return jobItem._id;
+    });
+    return JobItems.find({_id: {$in: jobItems}});
+  } else {
+    return [];
+  }
 };
 
 component.state.ings = function() {
-  return LocalMenuIngsAndPreps.find({"type": "ings", "menu": this.id});
-};
-
-component.prototype.onViewRendered = function() {
-  this.menu = MenuItems.findOne(this.id);
-  var ings = this.menu.ingredients;
-  var ids = [];
-  if(ings.length > 0) {
-    ings.forEach(function(ing) {
-      if(ids.indexOf(ings._id) < 0) {
-        ids.push(ing._id);
-      }
+  if(this.get('menu') && this.get('menu').ingredients) {
+    var ingredients = this.get('menu').ingredients;
+    ingredients = _.map(ingredients, function (ingredient) {
+      return ingredient._id;
     });
+    return Ingredients.find({_id: {$in: ingredients}});
+  } else {
+    return [];
   }
-  if(ids.length > 0) {
-    Meteor.subscribe("ingredients", ids);
-  }
-  Session.set("goBackMenu", null);
 };
