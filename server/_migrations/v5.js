236de354e0a27a9039bbb189ea3875cebc3f6def
YuraSrohiy <yuranesh@mail.ru>
2015-10-12 16:37:27 +0300

    formating

--- server/_migrations/v5.js
+++ server/_migrations/v5.js
@@ -14,16 +14,25 @@ Migrations.add({
                 query.address = "some address";
             }
             if(!location.pos){
-                query.pos = {
-                  key: "",
-                  secret: "",
-                  host:""
-                };
+                if(location.name = "Default location"){
+                    query.pos = {
+                        key: "a903d4c06d424a07ad3de99d3eec673c",
+                        secret: "a3f4b1f2b3a04d8fb7a277267b33086f0e0cbf2dafa444c7a387bf84cf63f034",
+                        host:"https://farmcafe.revelup.com"
+                    };
+                }else {
+                    query.pos = {
+                        key: "",
+                        secret: "",
+                        host: ""
+                    };
+                }
             }
             query.openingTime = moment({hours:8, minutes:0}).toDate();
             query.closingTime = moment({hours:17, minutes:0}).toDate();
             Locations.update({_id:location._id}, {$set:query, $unset:{status:""}});
         });
         Areas.update({}, {$unset:{status:""}}, {multi: true});
+
     }
 });
