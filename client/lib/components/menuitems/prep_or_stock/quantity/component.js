e56d422b5291539b0d335068eca5506de266422e
alonoslav <vadym.vorobel@gmail.com>
2015-10-13 16:02:05 +0300

    Small fixes

--- client/lib/components/menuitems/prep_or_stock/quantity/component.js
+++ client/lib/components/menuitems/prep_or_stock/quantity/component.js
@@ -31,12 +31,7 @@ component.prototype.onItemRendered = function() {
         var ing = $(this).data("pk");
         var type = $(this).data("itemtype");
         newValue = parseFloat(newValue);
-        if(newValue == newValue) {
-          newValue = newValue;
-        } else {
-          newValue = 0;
-        }
-        updateLocalData(ing, type, newValue, menu);
+        newValue = !isNaN(newValue) ? newValue : 0;
         if(type == "ings") {
           Meteor.call("addMenuIngredients", menu, [{"_id": ing, "quantity": newValue}], function(err) {
             if(err) {
@@ -61,8 +56,4 @@ component.prototype.onItemRendered = function() {
       }
     }
   });
-};
-
-function updateLocalData(_id, type, qty, menu) {
-  LocalMenuIngsAndPreps.update({"_id": _id, "menu": menu, "type": type}, {$set: {"quantity": qty}});
 };
