0849ff2264b09cb8eee960396640ae86278e3b5b
Taras Dolynyuk <trsdln@gmail.com>
2015-10-15 14:27:54 +0300

    Remove mail url from code

--- server/lib/config.js
+++ server/lib/config.js
@@ -9,7 +9,3 @@ if (Meteor.settings.developmentServers.indexOf(process.env.ROOT_URL) > -1) {
   process.env.NODE_ENV = "development";
   delete process.env.MAIL_URL;
 }
-
-if(process.env.NODE_ENV == 'production') {
-  process.env.MAIL_URL = 'smtp://postmaster%40herochef.com.au:Bertaroo724@smtp.mailgun.org:587/';
-}
