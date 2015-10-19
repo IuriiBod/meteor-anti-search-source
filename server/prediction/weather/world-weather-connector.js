fa48651add0bf21de9f1d8902b86292771157b46
YuraSrohiy <yuranesh@mail.ru>
2015-10-19 13:25:12 +0300

    configured WorldWeather connector

--- server/prediction/weather/world-weather-connector.js
+++ server/prediction/weather/world-weather-connector.js
@@ -15,16 +15,47 @@ WorldWeather = function WorldWeather(country, city) {
  * Base method used to access world weather API
  * @private
  */
-WorldWeather.prototype._httpQuery = function () {
+WorldWeather.prototype._httpQuery = function (route, params) {
+  var defaultParams = {
+    format: "json",
+    tp:3,
+    key: Meteor.settings.WorldWeather.KEY
+  };
 
+  var allParams = _.extend(defaultParams, params);
+  try {
+    var url = Meteor.settings.WorldWeather.HOST;
+    var res = HTTP.get(url + route, {
+      params: allParams
+    });
+    return res.data;
+  } catch (err) {
+    logger.error('WorldWeather Access Error', {params: params, error: err});
+    return false;
+  }
 };
 
 /**
  *
  * @param date
  */
-WorldWeather.prototype.getHistorical = function (date) {
-
+WorldWeather.prototype.getHistorical = function (fromDate, toDate) {
+  var startDate = moment(fromDate).format("YYYY-MM-DD");
+  var endDate = moment(toDate).format("YYYY-MM-DD");
+  var data = this._httpQuery("past-weather.ashx",{
+    q:this._city+","+this._country,
+    date: startDate,
+    endDate: endDate
+  });
+  var res = [];
+  _.each(data.data.weather, function (item) {
+    res.push({
+      date: item.date,
+      temp: item.hourly[5].tempC,
+      main: item.hourly[5].weatherDesc[0].value
+    });
+  });
+  return res;
 };
 
 
@@ -32,5 +63,18 @@ WorldWeather.prototype.getHistorical = function (date) {
  *
  */
 WorldWeather.prototype.getForecast = function () {
-
+  var today = moment().format("YYYY-MM-DD");
+  var data = this._httpQuery("weather.ashx",{
+    q:this._city+","+this._country
+  });
+  var res = [];
+  _.each(data.data.weather, function (item) {
+    res.push({
+      date: item.date,
+      temp: item.hourly[5].tempC,
+      main: item.hourly[5].weatherDesc[0].value,
+      icon: item.hourly[5].weatherIconUrl[0].value
+    });
+  });
+  return res;
 };
