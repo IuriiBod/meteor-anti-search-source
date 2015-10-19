6963519fd4a210bcc690baf38a4a04c1c1e47b14
Taras Dolynyuk <trsdln@gmail.com>
2015-10-19 12:56:57 +0300

    Remove redundant logs

--- server/prediction/weather/publishers.js
+++ server/prediction/weather/publishers.js
@@ -2,8 +2,6 @@ Meteor.publish('weatherForecast', function (year, week) {
   check(year, Number);
   check(year, Number);
 
-  var date = HospoHero.dateUtils.getDateByWeekDate({year: year, week: week});
-
   var haveAccess = HospoHero.perms.canUser('viewForecast')(this.userId);
   if (!haveAccess) {
     this.error(new Meteor.Error(403, 'Access Denied'));
@@ -14,9 +12,8 @@ Meteor.publish('weatherForecast', function (year, week) {
 
   OpenWeatherMap.updateWeatherForecastForLocation(locationId);
 
+  var date = HospoHero.dateUtils.getDateByWeekDate({year: year, week: week});
   var weekRange = TimeRangeQueryBuilder.forWeek(date);
-
-  logger.warn('publish weather forecast for: ', date, ' date range: ', weekRange);
 
   return WeatherForecast.find({date: weekRange, locationId: locationId});
 });
