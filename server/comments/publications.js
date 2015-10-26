04933ad7dadbf1b61f6eecea3b0244a85389bc20
alonoslav <vadym.vorobel@gmail.com>
2015-10-26 15:29:48 +0200

    Fixing publications of the dashboard

--- server/comments/publications.js
+++ server/comments/publications.js
@@ -1,16 +1,29 @@
-Meteor.publish('comments', function (ref) {
-  if(this.userId) {
-    var query = {
-      "reference": ref,
-      "relations.areaId": HospoHero.getCurrentAreaId(this.userId)
-    };
+Meteor.publishComposite('comments', function (ref) {
+  return {
+    find: function() {
+      if(this.userId) {
+        var query = {
+          "reference": ref,
+          "relations.areaId": HospoHero.getCurrentAreaId(this.userId)
+        };
 
-    logger.info("Comments published", ref);
-    return Comments.find(query, {
-      sort: {"createdOn": -1},
-      limit: 10
-    });
-  } else {
-    this.ready();
+        logger.info("Comments published", ref);
+        return Comments.find(query, {
+          sort: {"createdOn": -1},
+          limit: 10
+        });
+      } else {
+        this.ready();
+      }
+    },
+    children: [
+      {
+        find: function(comment) {
+          return Meteor.users.find({
+            _id: comment.createdBy
+          });
+        }
+      }
+    ]
   }
 });
