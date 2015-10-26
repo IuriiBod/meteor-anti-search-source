3475f5e4a821de8dd8a8ea0f8a1d8dc9b3edd01e
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-10-26 14:56:22 +0200

    Subscription from components/dashboad/clock moved out to router

--- both/lib/routes/home.js
+++ both/lib/routes/home.js
@@ -1,8 +1,8 @@
 //--------------------HOME
-Router.route("/", {
-  name: "home",
-  template: "home",
-  path: "/",
+Router.route('/', {
+  name: 'home',
+  template: 'home',
+  path: '/',
   waitOn: function() {
     if(Meteor.userId()) {
       return [
@@ -12,7 +12,8 @@ Router.route("/", {
         Meteor.subscribe('shifts', 'opened'),
         Meteor.subscribe('sections'),
         Meteor.subscribe('comments', Meteor.userId()),
-        Meteor.subscribe('newsfeeds')
+        Meteor.subscribe('newsfeeds'),
+        Meteor.subscribe('daily', moment().format('YYYY-MM-DD'), Meteor.userId())
       ];
     }
   },
