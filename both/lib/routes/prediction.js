ed6352567e89c80feda8c1b5499a752152bf14bd
YuraSrohiy <yuranesh@mail.ru>
2015-10-27 17:17:18 +0200

    changed url in category dropdown

--- both/lib/routes/prediction.js
+++ both/lib/routes/prediction.js
@@ -3,13 +3,11 @@ Router.route('/roster/prediction/:year/:week/:category', {
   template: "salesPredictionPage",
   waitOn: function () {
     return [
-
       this.subscribe('organizationInfo'),
       this.subscribe('weatherForecast', parseInt(this.params.year), parseInt(this.params.week)),
       this.subscribe('areaMenuItems', this.params.category),
       this.subscribe('allCategories'),
       this.subscribe("dailySales", parseInt(this.params.year), parseInt(this.params.week))
-
     ];
   },
   data: function () {
