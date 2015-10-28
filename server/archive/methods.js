d2cbe7319f0521603346f18a4bb7e3c041347deb
YuraSrohiy <yuranesh@mail.ru>
2015-10-28 10:37:16 +0200

    little archiving refactor

--- server/archive/methods.js
+++ server/archive/methods.js
@@ -4,17 +4,13 @@ Meteor.methods({
       var res = !location.archived;
       Locations.update({_id: location._id}, {$set:{archived: res}});
       var areas = Areas.find({locationId: location._id}).fetch();
-      _.each(areas, function (area) {
-        Meteor.call("switchArchiveArea", area, res);
-      });
     }else{
       throw new Meteor.Error("NOT ORGANIZATION OWNER");
     }
   },
   "switchArchiveArea": function(area, toSet){
     if(HospoHero.isOrganizationOwner()){
-      var canArchive = !Locations.findOne({_id: area.locationId}).archived;
-      if(typeof(toSet)==="undefined" && canArchive){
+      if(typeof(toSet)==="undefined"){
         var res = !area.archived;
         Areas.update({_id: area._id}, {$set:{archived: res}});
       }else if(typeof(toSet) === "boolean"){
