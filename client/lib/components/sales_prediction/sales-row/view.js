9ae0da598a069201ffc56b8691dd9be27ec6f169
YuraSrohiy <yuranesh@mail.ru>
2015-10-29 18:08:35 +0200

    deleted useless onRendered

--- client/lib/components/sales_prediction/sales-row/view.js
+++ client/lib/components/sales_prediction/sales-row/view.js
@@ -1,6 +1,1 @@
-Template.predictionSalesRow.onRendered(function () {
-  var maxItemsToHideBtn = 10;
-  if(MenuItems.find().count()<=maxItemsToHideBtn){
-    $("#loadMoreBtn").addClass("hide");
-  }
-});
+
