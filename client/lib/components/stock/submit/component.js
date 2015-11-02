00d115193bd303484326856be5348242e51e9892
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-11-02 15:25:56 +0200

    Component submitIngredient can be used as modal or as simple component

--- client/lib/components/stock/submit/component.js
+++ client/lib/components/stock/submit/component.js
@@ -1,15 +1,15 @@
-var component = FlowComponents.define('submitIngredient', function(props) {
+var component = FlowComponents.define('submitIngredientBody', function(props) {
+    var isModal = props.isModal || false;
+    this.set('isModal', isModal);
 });
 
 component.action.submit = function(event, info) {
-  Meteor.call("createIngredients", info, HospoHero.handleMethodResult(function() {
-    IngredientsListSearch.cleanHistory();
-    IngredientsListSearch.search("", {"limit": 10});
-    $(event.target).find("[type=text]").val("");
-    $("#addIngredientModal").modal("hide");
-  }));
+    Meteor.call("createIngredients", info, HospoHero.handleMethodResult(function() {
+        IngredientsListSearch.cleanHistory();
+        IngredientsListSearch.search("", {"limit": 10});
+    }));
 };
 
 component.state.suppliers = function() {
-  return Suppliers.find({}, {sort: {"name": 1}});
+    return Suppliers.find({}, {sort: {"name": 1}});
 };
--- client/lib/components/stock/submit/view.js
+++ client/lib/components/stock/submit/view.js
@@ -1,43 +1,51 @@
-Template.submitIngredient.events({
-  'submit form': function(event) {
-    event.preventDefault();
+Template.submitIngredientBody.events({
+    'submit #addIngredientBtn': function (event) {
+        event.preventDefault();
+    },
 
-    var fields = [
-      'code',
-      {
-        name: 'desc',
-        newName: 'description'
-      },
-      {
-        name: 'supplier',
-        newName: 'suppliers'
-      },
-      'portionOrdered',
-      'portionUsed',
-      {
-        name: 'costPerPortion',
-        parse: 'float',
-        type: 'number'
-      },
-      {
-        name: 'unitSize',
-        parse: 'float',
-        type: 'number'
-      }
-    ];
+    'submit #submitIngredientForm': function(event) {
+        console.log('submit event');
+        event.preventDefault();
 
-    var info = HospoHero.misc.getValuesFromEvent(event, fields, true);
+        var fields = [
+            'code',
+            {
+                name: 'desc',
+                newName: 'description'
+            },
+            {
+                name: 'supplier',
+                newName: 'suppliers'
+            },
+            'portionOrdered',
+            'portionUsed',
+            {
+                name: 'costPerPortion',
+                parse: 'float',
+                type: 'number'
+            },
+            {
+                name: 'unitSize',
+                parse: 'float',
+                type: 'number'
+            }
+        ];
 
-    if(!info.code) {
-      return HospoHero.error("You need to add a code");
+        var info = HospoHero.misc.getValuesFromEvent(event, fields, true);
+
+        if(!info.code) {
+            return HospoHero.error("You need to add a code");
+        }
+
+        if(!info.description) {
+            return HospoHero.error("You need to a description");
+        }
+
+        info.costPerPortion = Math.round(info.costPerPortion * 100)/100;
+
+        FlowComponents.callAction('submit', event, info);
+
+        $(event.target).find("[type=text]").val("");
+        $("#addIngredientModal").modal("hide");
     }
-
-    if(!info.description) {
-      return HospoHero.error("You need to a description");
-    }
-
-    info.costPerPortion = Math.round(info.costPerPortion * 100)/100;
-
-    FlowComponents.callAction('submit', event, info);
-  }
 });
