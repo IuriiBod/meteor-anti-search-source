39e2cc3c934f162c0c455c4bf1d39626b3af3e5c
Taras Dolynyuk <trsdln@gmail.com>
2015-11-03 17:10:03 +0200

    Production logger: replace file transport to papertrail

--- server/lib/config/logger.js
+++ server/lib/config/logger.js
@@ -7,20 +7,22 @@ var consoleTransportOptions = {
   humanReadableUnhandledException: true
 };
 
-var fileTransportOptions = {
-  name: 'file.error',
-  filename: '/root/hospo_hero.log',
-  colorize: true,
-  level: Meteor.settings.logLevel || 'warn',
-  levels: {debug: 0, info: 1, warn: 2, error: 3},
-  colors: {debug: 'blue', info: 'green', warn: 'orange', error: 'red'},
+var papertrailTransportOptions = {
+  host: 'logs3.papertrailapp.com',
+  port: 35920,
+  logFormat: function (level, message) {
+    return '[' + level + '] ' + message;
+  },
+  inlineMeta: true,
   json: true,
+  colorize: true,
   handleExeptions: true
 };
 
+
 // Add & configure transport
 if (HospoHero.isProductionMode()) {
-  logger.addTransport('file', fileTransportOptions);
+  logger.addTransport('papertrail', papertrailTransportOptions);
 } else {
   logger.addTransport('console', consoleTransportOptions);
 }
