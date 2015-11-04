4b8ea0dbc1983fa7db0eeb6c0c1ebe1d2b54ec6a
Taras Dolynyuk <trsdln@gmail.com>
2015-11-04 18:18:10 +0200

    Refactored prediction publishers

--- server/prediction/publications.js
+++ server/prediction/publications.js
@@ -1,27 +1,47 @@
+var checkForecastPrermssion = function (subscribtion) {
+  var haveAccess = HospoHero.canUser('view forecast', subscribtion.userId);
+  if (!haveAccess) {
+    subscribtion.error(new Meteor.Error(403, 'Access Denied'));
+  }
+  return haveAccess;
+};
+
+
 Meteor.publish('dailySales', function (weekRange, areaId) {
   check(weekRange, HospoHero.checkers.WeekRange);
 
-  var haveAccess = HospoHero.canUser('view forecast', this.userId);
-  if (!haveAccess) {
-    this.error(new Meteor.Error(403, 'Access Denied'));
+  if (checkForecastPrermssion(this)) {
+    logger.info('Daily sales subscription', weekRange);
+
+    return DailySales.find({'relations.areaId': areaId, date: weekRange});
   }
-
-  logger.info('Daily sales subscription', weekRange);
-
-  return DailySales.find({'relations.areaId': areaId, date: weekRange});
 });
 
 
 Meteor.publish('areaMenuItemsInfiniteScroll', function (limit, areaId) {
   check(limit, Number);
 
-  var haveAccess = HospoHero.canUser('view forecast', this.userId);
-  if (!haveAccess) {
-    this.error(new Meteor.Error(403, 'Access Denied'));
+  if (checkForecastPrermssion(this)) {
+    logger.info('Menu items for forecast subscription', {limit: limit});
+
+    var query = HospoHero.prediction.getMenuItemsForPredictionQuery({'relations.areaId': areaId});
+
+    return MenuItems.find(query, {limit: limit});
   }
+});
 
-  logger.info('Menu items for forecast subscription', {limit: limit});
-  var query = HospoHero.prediction.getMenuItemsForPredictionQuery({'relations.areaId': areaId});
 
-  return MenuItems.find(query, {limit: limit});
+Meteor.publish('weatherForecast', function (weekRange, areaId) {
+  check(weekRange, HospoHero.checkers.WeekRange);
+
+  if (checkForecastPrermssion(this)) {
+    logger.info('Weather subscribe ', weekRange);
+
+    var currentArea = Areas.findOne({_id: areaId});
+    var locationId = currentArea.locationId;
+
+    new WeatherManager(locationId).updateForecast();
+
+    return WeatherForecast.find({date: weekRange, locationId: locationId});
+  }
 });
