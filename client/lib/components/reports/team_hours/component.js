4282f04b50ea07eeff3446147ccb77f8ae584210
alonoslav <vadym.vorobel@gmail.com>
2015-11-06 12:31:50 +0200

    Removing limits

--- client/lib/components/reports/team_hours/component.js
+++ client/lib/components/reports/team_hours/component.js
@@ -3,8 +3,6 @@ var component = FlowComponents.define("teamHours", function (props) {
   this.weekDate = HospoHero.misc.getWeekDateFromRoute(Router.current());
   this.set('tableViewMode', 'shifts');
   this.set('searchText', '');
-  this.set('limit', 5);
-  this.count = 0;
 });
 
 component.state.weekDays = function () {
@@ -12,7 +10,6 @@ component.state.weekDays = function () {
 };
 
 component.state.users = function () {
-  var limit = this.get('limit');
   var query = {};
   var searchText = this.get('searchText');
   if(searchText) {
@@ -25,13 +22,6 @@ component.state.onKeyUp = function () {
   var self = this;
   return function(searchText) {
     self.set('searchText', searchText);
-  }
-};
-
-component.state.onChange = function () {
-  var self = this;
-  return function(limit) {
-    self.set('limit', parseInt(limit));
   }
 };
 
