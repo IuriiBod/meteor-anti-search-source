1572bb05d9c7e2d5766262b0b5c0e9f7672bae3b
alonoslav <vadym.vorobel@gmail.com>
2015-11-07 03:02:49 +1100

    Fixing ..(

--- server/shifts/functions.js
+++ server/shifts/functions.js
@@ -10,7 +10,7 @@ Meteor.methods({
       throw new Meteor.Error("You have no permissions to Clock In/Clock Out");
     }
     check(id, HospoHero.checkers.ShiftId);
-    Shifts.update({"_id": id}, {$set: {"status": "started", "startedAt": new Date().getTime()}});
+    Shifts.update({"_id": id}, {$set: {"status": "started", "startedAt": new Date()}});
     logger.info("Shift started", {"shiftId": id, "worker": user._id});
   },
 
@@ -21,7 +21,7 @@ Meteor.methods({
     }
     check(id, HospoHero.checkers.ShiftId);
     if (Shifts.findOne({"_id": id}).status === 'started'){
-      Shifts.update({"_id": id}, {$set: {"status": "finished", "finishedAt": new Date().getTime()}});
+      Shifts.update({"_id": id}, {$set: {"status": "finished", "finishedAt": new Date()}});
       logger.info("Shift ended", {"shiftId": id, "worker": user._id});
     }
 
