acd047332fed15a93e4346b3bd12592be5b8a04a
Taras Dolynyuk <trsdln@gmail.com>
2015-11-06 16:25:43 +0200

    Improved require login hook

--- both/lib/routes/_hooks.js
+++ both/lib/routes/_hooks.js
@@ -1,18 +1,52 @@
+var RoutePermissionChecker = function (routeInstance) {
+  this._user = Meteor.user();
+  this._routeName = routeInstance.route.getName();
+};
+
+RoutePermissionChecker.prototype.isOrganizationOwner = function () {
+  return !!Organizations.findOne({owner: this._user._id});
+};
+
+RoutePermissionChecker.prototype.checkRoutePermissions = function () {
+  return this._checkCurrentAreaNotArchived() && this._checkUserPermissionForRoute();
+};
+
+RoutePermissionChecker.prototype._checkUserPermissionForRoute = function () {
+  var permission = this._permissionsByRouteName[this._routeName];
+  return permission && HospoHero.canUser(permission, this._user._id) || !permission;
+};
+
+RoutePermissionChecker.prototype._checkCurrentAreaNotArchived = function () {
+  return this._user.currentAreaId && Areas.findOne({_id: this._user.currentAreaId, archived: {$ne: "true"}});
+};
+
+RoutePermissionChecker.prototype.checkIsUserInOrganization = function () {
+  return this._user.relations && this._user.relations.organizationId;
+};
+
+RoutePermissionChecker.prototype._permissionsByRouteName = {
+  // route_name : 'permission to check',
+  teamHours: 'edit roster',
+  currentStocks: 'edit roster'
+  //todo: add other routes and their permissions
+};
+
+
 var requireLogIn = function () {
   if (Meteor.loggingIn()) {
     this.render(this.loadingTemplate);
   } else if (Meteor.userId()) {
-    var user = Meteor.user();
+    var permissionChecker = new RoutePermissionChecker(this);
 
-    if (user.relations && user.relations.organizationId) {
-      if (user.currentAreaId && Areas.findOne({_id: user.currentAreaId, archived: {$ne: "true"}})) {
+    if (permissionChecker.checkIsUserInOrganization()) {
+      if (permissionChecker.checkRoutePermissions()) {
         return this.next();
       } else {
         Router.go("home");
         return this.next();
       }
     } else {
-      if (Organizations.findOne({owner: Meteor.userId()})) {
+      if (permissionChecker.isOrganizationOwner()) {
         return this.next();
       } else {
         Router.go('createOrganization');
