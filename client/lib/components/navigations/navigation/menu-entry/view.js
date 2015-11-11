246dc8d4e9916365af26c17ea92d1dcf295efdf5
YuraSrohiy <yuranesh@mail.ru>
2015-11-11 16:53:08 +0200

    refactoring

--- client/lib/components/navigations/navigation/menu-entry/view.js
+++ client/lib/components/navigations/navigation/menu-entry/view.js
@@ -1,10 +1,29 @@
 Template.menuEntry.events({
   'click .menu-entry-title': function (event, tmpl) {
-    $('.nav-first-level-item').removeClass('show-sub-menu');
-    tmpl.$('.nav-first-level-item').addClass('show-sub-menu');
-    var media_query = window.matchMedia('(max-width: 767px)');
-    if (media_query.matches && !$(event.currentTarget).next()[0]) {
-      $('#wrapper').removeClass('force-show-sidebar').addClass('force-hide-sidebar');
+    showSubMenu(tmpl);
+    if (isMobile() && !haveSubMenu(event)) {
+      hideSideMenu();
     }
   }
 });
+
+//HELPING FUNCTIONS
+
+var isMobile = function () {
+  return window.matchMedia('(max-width: 767px)').matches;
+};
+
+var haveSubMenu = function (event) {
+  return !!$(event.currentTarget).next()[0];
+};
+
+var showSubMenu = function (tmpl) {
+  $('.nav-first-level-item').removeClass('show-sub-menu');  //removing class from all menus
+  tmpl.$('.nav-first-level-item').addClass('show-sub-menu'); //adding class to clicked menu
+};
+
+
+var hideSideMenu = function () {
+  $('#wrapper').removeClass('force-show-sidebar').addClass('force-hide-sidebar');
+};
+
