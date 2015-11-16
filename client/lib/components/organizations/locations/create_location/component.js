dd356f21dbd5d583e8ab4eee7a5501998e4e0054
Taras Dolynyuk <trsdln@gmail.com>
2015-11-17 02:51:23 +1100

    Refactoring of location flytous

--- client/lib/components/organizations/locations/create_location/component.js
+++ client/lib/components/organizations/locations/create_location/component.js
@@ -1,6 +1,25 @@
 var component = FlowComponents.define("createLocation", function (props) {
 });
 
-component.action.submit = function (doc) {
-  Meteor.call("createLocation", doc, HospoHero.handleMethodResult());
+component.state.onLocationSubmit = function () {
+  var self = this;
+
+  return function (newLocationDoc, event) {
+    Meteor.call("createLocation", newLocationDoc, HospoHero.handleMethodResult(function () {
+      self.closeFlyoutByEvent(event);
+    }));
+  };
+};
+
+component.state.onCancel = function () {
+  var self = this;
+
+  return function (event) {
+    self.closeFlyoutByEvent(event);
+  };
+};
+
+component.prototype.closeFlyoutByEvent = function (event) {
+  var flyout = FlyoutManager.getInstanceByElement(event.target);
+  flyout.close();
 };
---
+++ client/lib/components/organizations/locations/edit-location-form/component.js
@@ -0,0 +1,38 @@
+var component = FlowComponents.define('editLocationForm', function (props) {
+  this.set('submitCaption', props.submitCaption);
+  this.set('locationDoc', props.locationDoc || {});
+  this.onSubmit = props.onSubmit;
+  this.onCancel = props.onCancel;
+
+  this.defaultOpeningTime = moment('8:00').toDate();
+  this.defaultClosingTime = moment('17:00').toDate();
+});
+
+component.state.timeZone = function () {
+  var locationDoc = this.get('locationDoc');
+  return locationDoc.timezone || TimezonePicker.detectedZone();
+};
+
+component.state.openingTime = function () {
+  var locationDoc = this.get('locationDoc');
+  return locationDoc.openingTime || this.defaultOpeningTime;
+};
+
+component.state.closingTime = function () {
+  var locationDoc = this.get('locationDoc');
+  return locationDoc.closingTime || this.defaultClosingTime;
+};
+
+
+component.action.submit = function (newLocationDoc) {
+  if (_.isFunction(this.onSubmit)) {
+    this.onSubmit(newLocationDoc);
+  }
+};
+
+
+component.action.cancel = function () {
+  if (_.isFunction(this.onCancel)) {
+    this.onCancel();
+  }
+};
