2f538eca3bbe4cc2d52a9ae4bf3234b7a3048f2f
Taras Dolynyuk <trsdln@gmail.com>
2015-11-18 12:14:40 +0200

    Fixed exception in edit roster

--- server/lib/method-error-agregator.js
+++ server/lib/method-error-agregator.js
@@ -1,21 +1,23 @@
-Meteor._registerMethodsFn = Meteor.methods;
+if (HospoHero.isProductionMode()) {
+  Meteor._registerMethodsFn = Meteor.methods;
 
-Meteor.methods = function (methodsObj) {
-  var wrapWithExceptionLogging = function (method) {
-    return function () {
-      try {
-        method.apply(this, arguments);
-      } catch (err) {
-        //log error and return it to client
-        logger.error(err);
-        throw err;
-      }
+  Meteor.methods = function (methodsObj) {
+    var wrapWithExceptionLogging = function (method) {
+      return function () {
+        try {
+          method.apply(this, arguments);
+        } catch (err) {
+          //log error and return it to client
+          logger.error(err);
+          throw err;
+        }
+      };
     };
+
+    Object.keys(methodsObj).forEach(function (methodName) {
+      methodsObj[methodName] = wrapWithExceptionLogging(methodsObj[methodName]);
+    });
+
+    Meteor._registerMethodsFn(methodsObj);
   };
-
-  Object.keys(methodsObj).forEach(function (methodName) {
-    methodsObj[methodName] = wrapWithExceptionLogging(methodsObj[methodName]);
-  });
-
-  Meteor._registerMethodsFn(methodsObj);
-};
+}
