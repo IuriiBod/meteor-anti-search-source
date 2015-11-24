5494bcb3bb9cc991dda0aa4106d615a5b036353d
Taras Dolynyuk <trsdln@gmail.com>
2015-11-24 17:46:22 +0200

    Improved ghost editable

--- client/lib/components/common/ghost-editable/view.js
+++ client/lib/components/common/ghost-editable/view.js
@@ -1,9 +1,33 @@
+Template.ghostEditable.onCreated(function () {
+  this.showSubmitButton = new ReactiveVar(false);
+
+  this.getInputValue = function () {
+    return this.$('.ghost-editable-input').val();
+  };
+
+  this.submitValue = function () {
+    var newValue = this.getInputValue();
+    FlowComponents.callAction('submitValue', newValue);
+  };
+});
+
+
+Template.ghostEditable.helpers({
+  showSubmitButton: function () {
+    return Template.instance().showSubmitButton.get();
+  }
+});
+
+
 Template.ghostEditable.events({
   'keyup .ghost-editable-input': function (event, tmpl) {
+    tmpl.showSubmitButton.set(true);
     if (event.keyCode === 13) {
-      var newValue = tmpl.$('.ghost-editable-input').val();
-
-      FlowComponents.callAction('submitValue', newValue);
+      tmpl.submitValue();
     }
+  },
+
+  'click .submit-value-button': function (event, tmpl) {
+    tmpl.submitValue();
   }
 });
