f19eaaeb82a02f50f22c9b26d4e3eb4dc51138a3
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-12-02 15:23:03 +0200

    Excluding unavailable time intervals

--- client/lib/components/roster/weekly-shift-roster/weekly-roster-day/shift-basic/time-editable/time-editable.js
+++ client/lib/components/roster/weekly-shift-roster/weekly-roster-day/shift-basic/time-editable/time-editable.js
@@ -40,55 +40,97 @@ Template.shiftBasicTimeEditable.onCreated(function () {
             return;
         }
 
-        unavailabileTimeIntervals = [];
+        var shiftUnavailableTimeIntervals = [];
 
         var shiftStartOfDayDate = moment(self.data.shift.startTime).startOf('day').toDate();
         var shiftEndOfDayDate = moment(self.data.shift.startTime).endOf('day').toDate();
 
-        var leaveRequests = LeaveRequests.find({
-            userId: assignedUserId,
-            startDate: {$lte: shiftStartOfDayDate},
-            endDate: {$gte: shiftEndOfDayDate}
-        }).fetch();
-
-        if (leaveRequests.length > 0) {
-            unavailabileTimeIntervals.push({
-                startDate: shiftStartOfDayDate,
-                endDate: shiftEndOfDayDate
-            });
-            return unavailabileTimeIntervals;
-        }
+        // This checking should be in method assign user, and moving shift through days
+        //// find leave request, that overlays current shift
+        //var leaveRequests = LeaveRequests.find({
+        //    userId: assignedUserId,
+        //    startDate: {$lte: shiftStartOfDayDate},
+        //    endDate: {$gte: shiftEndOfDayDate}
+        //}).fetch();
+        //
+        //// if exist at least one approved leave request,
+        //if (leaveRequests.length > 0) {
+        //    unavailabileTimeIntervals.push({
+        //        startDate: shiftStartOfDayDate,
+        //        endDate: shiftEndOfDayDate
+        //    });
+        //    return unavailabileTimeIntervals;
+        //}
 
         var unavailabilities = Meteor.users.findOne({_id: assignedUserId}).unavailabilities || [];
 
         unavailabilities.every(function (unavailability) {
-            if (unavailability.startDate >= shiftStartOfDayDate && unavailability.endDate <= shiftEndOfDayDate) {
-                if (unavailability.startDate.getTime() == unavailability.endDate.getTime()) {
-                    unavailabileTimeIntervals.push({
-                        startDate: shiftStartOfDayDate,
-                        endDate: shiftEndOfDayDate
-                    });
-                    return false;
-                } else {
-                    var interval = {
-                        startDate: unavailability.startDate,
-                        endDate: unavailability.endDate
-                    }
-                    unavailabileTimeIntervals.push(interval);
-                    return true;
-                }
+            var isUnavailabilityForToday = unavailability.repeat != 'never' && self.checkRepeatUnavailability(unavailability)
+                || unavailability.startDate >= shiftStartOfDayDate && unavailability.endDate <= shiftEndOfDayDate;
+
+            if (isUnavailabilityForToday) {
+                return self.getUnavailabilityInterval(unavailability, shiftUnavailableTimeIntervals);
             }
             return true;
         });
 
-        return unavailabileTimeIntervals;
-    }
+        return shiftUnavailableTimeIntervals;
+    };
+
+    self.checkRepeatUnavailability = function (unavailability) {
+        var unavailabilityMoment = moment(unavailability.startDate).startOf('day');
+        var shiftMoment = moment(self.data.shift.startDate).startOf('day');
+
+        // check day of week repeat
+        if (unavailabilityMoment.day() == shiftMoment.day()) {
+            return true;
+        }
+
+        // check day of month repeat
+        var diffBetweenMoments = shiftMoment.diff(unavailabilityMoment, 'month');
+
+        unavailabilityMoment.add(diffBetweenMoments, 'month');
+
+        if (unavailabilityMoment.date() == shiftMoment.date() && unavailabilityMoment.month() == shiftMoment.month()) {
+            return true;
+        }
+        return false;
+    };
+
+    self.getUnavailabilityInterval = function (unavailabilityItem, shiftUnavailableTimeIntervals) {
+        var shiftUnavailabilityStartMoment = moment(self.data.shift.startDate);
+        var shiftUnavailabilityEndMoment = moment(self.data.shift.startDate);
+
+        // if unavailability for all day
+        if (unavailabilityItem.startDate.getTime() == unavailabilityItem.endDate.getTime()) {
+            shiftUnavailabilityStartMoment.startOf('day');
+            shiftUnavailabilityEndMoment.endOf('day');
+
+            shiftUnavailableTimeIntervals.push([
+                shiftUnavailabilityStartMoment,
+                shiftUnavailabilityEndMoment
+            ]);
+            return false;
+        } else {
+            // or, for part of day
+            var unavailabilityStartTime = moment(unavailabilityItem.startDate);
+            var unavailabilityEndTime = moment(unavailabilityItem.endDate);
+            shiftUnavailabilityStartMoment.hours(unavailabilityStartTime.hours()).minutes(unavailabilityStartTime.minutes());
+            shiftUnavailabilityEndMoment.hours(unavailabilityEndTime.hours()).minutes(unavailabilityEndTime.minutes());
+
+            var interval = [
+                shiftUnavailabilityStartMoment,
+                shiftUnavailabilityEndMoment
+            ];
+            shiftUnavailableTimeIntervals.push(interval);
+            return true;
+        }
+    };
 });
 
 Template.shiftBasicTimeEditable.onRendered(function () {
     var self = this;
     var unavailabileTimeIntervals = self.getUserUnavailableTimeIntervals();
-    console.log('unavailabileTimeIntervals:\n', unavailabileTimeIntervals);
 
     var DATE_TIME_PICKER_FORMAT = 'HH:mm';
     var DATE_TIME_PICKER_STEP = 10;
@@ -97,13 +139,15 @@ Template.shiftBasicTimeEditable.onRendered(function () {
         format: DATE_TIME_PICKER_FORMAT,
         dayViewHeaderFormat: DATE_TIME_PICKER_FORMAT,
         stepping: DATE_TIME_PICKER_STEP,
-        defaultDate: self.data.shift.startTime
+        defaultDate: self.data.shift.startTime,
+        disabledTimeIntervals: unavailabileTimeIntervals
     });
     self.$('.end-time').datetimepicker({
         format: DATE_TIME_PICKER_FORMAT,
         dayViewHeaderFormat: DATE_TIME_PICKER_FORMAT,
         stepping: DATE_TIME_PICKER_STEP,
         defaultDate: self.data.shift.endTime,
+        disabledTimeIntervals: unavailabileTimeIntervals,
 
         useCurrent: false //Important! See issue #1075
     });
@@ -113,11 +157,14 @@ Template.shiftBasicTimeEditable.onRendered(function () {
 
     var MINIMUM_SHIFT_DURATION = 30;
     self.$(".start-time").on("dp.change", function (e) {
-        var newMinDate = e.date.add(MINIMUM_SHIFT_DURATION, 'minutes');
-        self.endTimePicker.minDate(newMinDate);
+        if (self.endTimePicker.date() < e.date) {
+            self.endTimePicker.date(e.date.add(MINIMUM_SHIFT_DURATION, 'minutes'));
+        }
+    });
 
-        if (self.endTimePicker.date() < self.endTimePicker.minDate()) {
-            self.endTimePicker.date(newMinDate);
+    self.$(".end-time").on("dp.change", function (e) {
+        if (self.startTimePicker.date() > e.date) {
+            self.startTimePicker.date(e.date.subtract(MINIMUM_SHIFT_DURATION, 'minutes'));
         }
     });
 });
