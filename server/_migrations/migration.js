838461f09c9b505166f98d79cf9a05ea5c881448
Taras Dolynyuk <trsdln@gmail.com>
2015-12-02 10:36:56 +0200

    Added migrations.utils

--- server/_migrations/migration.js
+++ server/_migrations/migration.js
@@ -1,11 +1,12 @@
-//var removeDatabase = function (dbName) {
-//  Areas.rawDatabase().dropCollection(dbName, function () {
-//  });
-//};
+Migrations.utils = {
+  removeDatabase: function (dbName) {
+    Areas.rawDatabase().dropCollection(dbName, function () {
+    });
+  }
+};
 
 if (!HospoHero.isDatabaseImportMode()) {
   Meteor.startup(function () {
-    //process.env.NO_MIGRATION - flag that enables run app without migrations
     console.log('Database version: ', Migrations.getVersion());
     Migrations.migrateTo('latest');
   });
