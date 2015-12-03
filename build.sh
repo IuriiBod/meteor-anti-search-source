f8ae7d8a6bdfeb20b60df7f553b597aaff4de9fc
Taras Dolynyuk <trsdln@gmail.com>
2015-12-03 15:25:19 +0200

    Removed build.sh from project

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -R ../hospohero-build
-meteor build ../hospohero-build --server=https://hospoherotesting.herokuapp.com
-keytool -genkey -alias hospohero -keyalg RSA -keysize 2048 -validity 10000
-cd ../hospohero-build/android/
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 unaligned.apk hospohero
-~/.meteor/android_bundle/android-sdk/build-tools/21.0.0/zipalign 4 unaligned.apk hospohero.apk
-rm ~/Desktop/hospohero.apk
-cp hospohero.apk ~/Desktop/hospohero.apk
