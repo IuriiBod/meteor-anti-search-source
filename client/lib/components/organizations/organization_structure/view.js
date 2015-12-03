d1289de8537f10eeb0b9136055bac7a3323388df
alonoslav <vadym.vorobel@gmail.com>
2015-12-03 09:26:49 +0200

    Removed raix:push

--- client/lib/components/organizations/organization_structure/view.js
+++ client/lib/components/organizations/organization_structure/view.js
@@ -10,8 +10,6 @@ Template.organizationStructure.events({
   'click .change-current-area': function(e) {
     e.preventDefault();
 
-    Meteor.call('userNotification', 'HospoHero notification', 'This is only the test notification');
-
     FlowComponents.callAction('changeDefaultArea', this._id);
     var routerParams = Router.current().params;
 
