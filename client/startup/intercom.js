7c8fc4e5727e080713f1541812af6f2f97736da3
Taras Dolynyuk <trsdln@gmail.com>
2015-12-03 13:14:56 +0200

    Small configuration changes

--- client/startup/intercom.js
+++ client/startup/intercom.js
@@ -1,17 +1,17 @@
 Meteor.startup(function () {
-  IntercomSettings.userInfo = function (user, info) {
-    if (!user.intercomHash) {
-      return false;
-    } else {
-      // add properties to the info object, for instance:
-      if (user.services && user.services.google) {
-        info.email = user.services.google.email;
-        info['name'] = user.services.google.given_name + ' ' + user.services.google.family_name;
-      } else {
-        info.email = user.emails[0].address;
-        info['name'] = user.username;
-      }
-      info['created_at'] = new Date(user.createdAt).getTime();
-    }
-  };
+  //IntercomSettings.userInfo = function (user, info) {
+  //  if (!user.intercomHash) {
+  //    return false;
+  //  } else {
+  //    // add properties to the info object, for instance:
+  //    if (user.services && user.services.google) {
+  //      info.email = user.services.google.email;
+  //      info['name'] = user.services.google.given_name + ' ' + user.services.google.family_name;
+  //    } else {
+  //      info.email = user.emails[0].address;
+  //      info['name'] = user.username;
+  //    }
+  //    info['created_at'] = new Date(user.createdAt).getTime();
+  //  }
+  //};
 });
