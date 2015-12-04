859db30d5209f286055d39af48d060775a5b68a0
alonoslav <vadym.vorobel@gmail.com>
2015-12-04 18:58:26 +0200

    Fixed bug with order type

--- client/lib/components/stocktake/ordering/supplier_filter/view.js
+++ client/lib/components/stocktake/ordering/supplier_filter/view.js
@@ -1,13 +1,6 @@
-Template.supplierFilter.events({
-  'click .activateSupplier': function(event) {
-    event.preventDefault();
-    FlowComponents.callAction('setActiveSupplier', this.toString());
-  },
-
-  'click .setOrder': function(event) {
-    event.preventDefault();
+Template.supplierFilter.onRendered(function () {
+  this.generateReceipts = function (orderType) {
     FlowComponents.callAction('getSupplier').then(function(supplier) {
-      var orderType = $(event.target).attr("data-type");
       var version = HospoHero.getParamsFromRoute(Router.current(), '_id');
       var address = null;
       var deliveryDate = moment().add(1, 'days').format('x');
@@ -23,5 +16,22 @@ Template.supplierFilter.events({
         Meteor.call("generateReceipts", version, supplier, info, HospoHero.handleMethodResult());
       }
     });
+  }
+});
+
+Template.supplierFilter.events({
+  'click .activateSupplier': function(event) {
+    event.preventDefault();
+    FlowComponents.callAction('setActiveSupplier', this.toString());
+  },
+
+  'click .orderEmailed': function(event, tmpl) {
+    event.preventDefault();
+    tmpl.generateReceipts('emailed');
+  },
+
+  'click .orderPhoned': function(event, tmpl) {
+    event.preventDefault();
+    tmpl.generateReceipts('phoned');
   }
 });
