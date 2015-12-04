39373d7fe44fb4847499f56dc80cb4f6771aceec
alonoslav <vadym.vorobel@gmail.com>
2015-12-04 14:15:44 +0200

    Added ghost editable select

--- client/lib/components/suppliers/profile/basics/component.js
+++ client/lib/components/suppliers/profile/basics/component.js
@@ -14,6 +14,27 @@ component.state.lastOrder = function () {
   return OrderReceipts.findOne({'supplier': this.get('supplier')._id}, {sort: {'date': -1}, limit: 1});
 };
 
+component.state.deliveryDays = function () {
+  return [
+    {value: 'sunday', text: 'Sunday'},
+    {value: 'monday', text: 'Monday'},
+    {value: 'tuesday', text: 'Tuesday'},
+    {value: 'wednesday', text: 'Wednesday'},
+    {value: 'thursday', text: 'Thursday'},
+    {value: 'friday', text: 'Friday'},
+    {value: 'saturday', text: 'Saturday'}
+  ];
+};
+
+component.state.onDeliveryDayChanged = function () {
+  var self = this;
+  return function (value) {
+    var supplier = self.get('supplier');
+    supplier.deliveryDay = value;
+    Meteor.call('editSupplier', supplier, HospoHero.handleMethodResult());
+  }
+};
+
 component.action.changeSupplierStatus = function () {
   var id = this.get('supplier').id;
   Meteor.call('activateReactivateSuppliers', id, HospoHero.handleMethodResult());
