326f404f982593ecc36f158c4171464141b66843
alonoslav <vadym.vorobel@gmail.com>
2015-12-04 11:24:22 +0200

    Fixed stocktakes page

--- client/lib/components/stocktake/master_list/list/component.js
+++ client/lib/components/stocktake/master_list/list/component.js
@@ -1,14 +1,31 @@
-var component = FlowComponents.define("stockTakeMasterList", function() {
-  this.onRendered(this.onListRender);
+var component = FlowComponents.define("stockTakeMasterList", function () {
 });
 
-component.state.listOfDates = function() {
-  return this.get("historyList");
-};
+component.state.listOfDates = function () {
+  var stocktakeList = [];
+  var stocktakes = Stocktakes.find({}, {
+    fields: {
+      date: 1,
+      counting: 1,
+      unitCost: 1,
+      version: 1
+    },
+    sort: {date: 1}
+  }).fetch();
 
-component.prototype.onListRender = function() {
-  var self = this;
-  Meteor.call("stockTakeHistory", HospoHero.handleMethodResult(function(list) {
-    self.set("historyList", list);
-  }));
+  stocktakes = _.groupBy(stocktakes, 'version');
+
+
+  Object.keys(stocktakes).forEach(function (stocktakeId) {
+    var stocktakeCost = _.reduce(stocktakes[stocktakeId], function (memo, stocktake) {
+      return memo + stocktake.counting * stocktake.unitCost;
+    }, 0);
+
+    stocktakeList.push({
+      _id: stocktakeId,
+      date: stocktakes[stocktakeId][0].date,
+      totalStockValue: stocktakeCost
+    });
+  });
+  return stocktakeList;
 };
