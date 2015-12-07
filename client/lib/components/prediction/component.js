f36c7b54c136f1fbec88f483f0322aebc2b09325
ahio <stepan.te.ua@gmail.com>
2015-12-07 10:59:46 +0200

    fixed load more button for forecast

--- client/lib/components/prediction/component.js
+++ client/lib/components/prediction/component.js
@@ -14,6 +14,31 @@ var component = FlowComponents.define("salesPrediction", function (props) {
 
 });
 
+component.prototype.newParamsToSearchData = function(dataHistory, text) {
+  var text = text ? text : "";
+  var count = dataHistory.length;
+  var lastItem = dataHistory[count - 1]['name'];
+  var category = Router.current().params.category;
+  var filter = [];
+  var selector = {
+    "limit": count + this.limitAdd,
+    "endingAt": lastItem
+  };
+  filter.push({
+    "status": "active",
+    "relations.areaId": HospoHero.getCurrentAreaId()
+  });
+  if (category && category.toLowerCase() != "all") {
+    filter.push({"category": category});
+  }
+  selector.filter = filter;
+
+  return {
+    text: text,
+    selector: selector
+  }
+};
+
 component.state.week = function () {
   var currentWeekDate = this.get('currentWeekDate');
   return HospoHero.dateUtils.getWeekDays(currentWeekDate);
@@ -39,30 +64,17 @@ component.state.allItemsLoaded = function () {
 
 component.action.loadMoreBtnClick = function (text) {
   var search = this.MenuItemsSearch;
+  var params = _.keys(search.history);
   this.clicks++;
-  if (search.history && search.history[text]) {
-    var dataHistory = search.history[text].data;
+  if (search.history && search.history[params]) {
+    var dataHistory = search.history[params].data;
     if (dataHistory.length >= this.maxHistoryLength) {
       search.cleanHistory();
-      var count = dataHistory.length;
-      var lastItem = dataHistory[count - 1]['name'];
-      var category = Router.current().params.category;
-      var filter = [];
-      var selector = {
-        "limit": count + this.limitAdd,
-        "endingAt": lastItem
-      };
-      filter.push({
-        "status": "active",
-        "relations.areaId": HospoHero.getCurrentAreaId()
-      });
-      if (category && category.toLowerCase() != "all") {
-        filter.push({"category": category});
-      }
-      selector.filter = filter;
-      search.search(text, selector);
+      var searchParams = this.newParamsToSearchData(dataHistory, text);
+      search.search(searchParams.text, searchParams.selector);
     }
   }
+
   if ((this.clicks * this.limitAdd) > dataHistory.length) {
     this.set("allMenuItemsLoaded", true);
   }
