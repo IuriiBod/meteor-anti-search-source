3909b24b4796d6684ee7218516e590878bb879b4
Taras Dolynyuk <trsdln@gmail.com>
2015-12-08 11:46:57 +0200

    Back state helpers in blaze-states

--- packages/blaze-states/blaze-states.js
+++ packages/blaze-states/blaze-states.js
@@ -4,28 +4,35 @@ Blaze.TemplateInstance.prototype.__stateKeyByName = function (stateName) {
   return '_' + stateName;
 };
 
-Blaze.TemplateInstance.prototype.__addHelperForState = function (stateName) {
-  var helperFn = (function () {
-    return this.get(stateName);
-  }).bind(this);
-
-  this.view.template.__helpers.set('state$' + stateName, helperFn);
-};
-
-
 Blaze.TemplateInstance.prototype.set = function (stateName, stateValue) {
   var stateInstance = this[this.__stateKeyByName(stateName)];
   if (!stateInstance) {
     this[this.__stateKeyByName(stateName)] = new ReactiveVar(stateValue);
-
-    //add register helper for new state
-    this.__addHelperForState(stateName);
   } else {
     stateInstance.set(stateValue);
   }
 };
 
+
 Blaze.TemplateInstance.prototype.get = function (stateName) {
   var stateInstance = this[this.__stateKeyByName(stateName)];
   return stateInstance && stateInstance.get();
+};
+
+
+//ugly hack from flow-components
+var originalLookup = Blaze.View.prototype.lookup;
+
+Blaze.View.prototype.lookup = function (name, options) {
+  if (/^state\$/.test(name)) {
+    var stateName = name.replace(/^state\$/, "");
+    var templateInstance = this.templateInstance();
+    var value = templateInstance.get(stateName);
+    if (value) {
+      return value;
+    } else {
+      return originalLookup.call(this, name, options);
+    }
+  }
+  return originalLookup.call(this, name, options);
 };
