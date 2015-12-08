5c5e63483e6d29f3c7b1eaa3b760b0471f05d0f4
alonoslav <vadym.vorobel@gmail.com>
2015-12-08 12:16:11 +0200

    Fixed timepicker component

--- client/lib/components/common/combo-time-editable/custom_combodate_editable/combo_time_element.js
+++ client/lib/components/common/combo-time-editable/custom_combodate_editable/combo_time_element.js
@@ -1,24 +1,40 @@
 Template.comboTimeElement.onCreated(function () {
+  /**
+   * Round decimal number to the nearest number with persistence
+   * 47 -> 45, 48 -> 50
+   * @param numberToRound
+   * @param persistence
+   */
+  this.roundToNearestNumber = function (numberToRound, persistence) {
+    // get the division remainder (47 % 50 = 2)
+    var remainder = numberToRound % persistence;
 
-  var defaultMoment = moment(this.data.value);
+    // get the sign of round operation (2 / 5 ~ 0; 3 / 5 ~ 1)
+    // if 0 - the sign of operation will be -; otherwise +
+    var roundingSign = Math.round(remainder / persistence);
+    return roundingSign ? numberToRound + remainder : numberToRound - remainder;
+  };
 
-  var hours = defaultMoment.hours();
-  var ampm = hours > 11 ? 'pm' : 'am';
-  hours = hours > 11 ? hours - 12 : hours;
-  var minutes = defaultMoment.minutes();
+  var defaultMoment = _.isDate(this.data.value) ? moment(this.data.value) : moment();
+  var hours = moment(defaultMoment).format('h');
+  var ampm = moment(defaultMoment).format('a');
+  var minutes = moment(defaultMoment).minutes();
+
+  var minuteStepping = this.data.minuteStepping || 1;
+  minutes = this.roundToNearestNumber(minutes, minuteStepping);
 
   this.timeObject = {
     hours: hours,
     minutes: minutes,
     ampm: ampm
-  }
+  };
 });
 
 Template.comboTimeElement.helpers({
   hours: function () {
     var hours = [];
 
-    for (var i = 0; i < 12; i++) {
+    for (var i = 1; i <= 12; i++) {
       var hour = {};
       hour.value = i;
       hour.selected = Template.instance().timeObject.hours == i ? 'selected' : '';
@@ -33,7 +49,7 @@ Template.comboTimeElement.helpers({
 
     for (var i = 0; i < 60; i = i + minuteStepping || 1) {
       var minute = {};
-      minute.value = i;
+      minute.value = i < 10 ? '0' + i : i;
       minute.selected = Template.instance().timeObject.minutes == i ? 'selected' : '';
 
       minutes.push(minute);
