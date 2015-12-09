c19c39347c6fc01f92461cd212a9f41f78759783
Taras Dolynyuk <trsdln@gmail.com>
2015-12-09 18:36:52 +0200

    Migration for shift improvement

--- server/_migrations/v26.js
+++
@@ -1,7 +0,0 @@
-Migrations.add({
-  version: 26,
-  name: "Remove old notifications",
-  up: function () {
-    Notifications.remove({relations: {$ne: null}});
-  }
-});
---
+++ server/_migrations/v37.js
@@ -0,0 +1,44 @@
+Migrations.add({
+  version: 37,
+  name: "Fix shifts times",
+  up: function () {
+    Locations.find({}).forEach(function (location) {
+
+      var convertToLocalMoment = function (date) {
+        return HospoHero.dateUtils.getDateMomentForLocation(moment(date), location);
+      };
+
+      var applyDateToTime = function (date, time) {
+        return moment(date).hours(time.hours()).minutes(time.minutes());
+      };
+
+      Shifts.find({'relations.locationId': location._id}).forEach(function (shift) {
+        var shiftMoment = convertToLocalMoment(shift.shiftDate).startOf('day');
+        delete shift.shiftDate;
+
+        var startTime = convertToLocalMoment(shift.startTime);
+        var endTime = convertToLocalMoment(shift.endTime);
+
+        shift.startTime = applyDateToTime(shiftMoment, startTime).toDate();
+        shift.endTime = applyDateToTime(shiftMoment, endTime).toDate();
+
+        var startedAt, finishedAt;
+
+        if (shift.startedAt && shift.finishedAt) {
+          startedAt = convertToLocalMoment(shift.startedAt);
+          finishedAt = convertToLocalMoment(shift.finishedAt);
+        }
+
+        delete shift.startedAt;
+        delete shift.finishedAt;
+
+        if (shift.startedAt && shift.finishedAt) {
+          shift.startedAt = applyDateToTime(shiftDate, startedAt).toDate();
+          shift.finishedAt = applyDateToTime(shiftDate, finishedAt).toDate();
+        }
+
+        Shifts.update({_id: shift._id}, shift);
+      });
+    });
+  }
+});
