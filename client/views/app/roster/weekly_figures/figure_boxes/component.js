1da86c5c31dfac304c438dd0c22dc270af8ef0a3
Taras Dolynyuk <trsdln@gmail.com>
2015-12-09 12:46:51 +0200

    roster weekly figures

--- client/views/app/roster/weekly_figures/figure_boxes/component.js
+++
@@ -1,7 +0,0 @@
-var component = FlowComponents.define("figureBoxes", function (props) {
-  this.figureBox = props.figureBoxDataHelper;
-});
-
-component.state.getFigureBoxDataHelper = function () {
-  return this.figureBox;
-};
