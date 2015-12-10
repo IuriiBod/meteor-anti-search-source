138b28a4ecc289a922674e8a9d4826d8897037d7
ahio <stepan.te.ua@gmail.com>
2015-12-10 10:11:07 +0200

    fixed event to save new area

--- client/views/app/stocktake/counting-view/counting-view-items/areas/new-area-modal/new-area-modal.js
+++ client/views/app/stocktake/counting-view/counting-view-items/areas/new-area-modal/new-area-modal.js
@@ -5,37 +5,35 @@ Template.newAreaModal.helpers({
 });
 
 Template.newAreaModal.events({
-  'click #savegeneralArea': function (event) {
+  'click #savegeneralArea': function (event, tmpl) {
     event.preventDefault();
     var name = $("#generalareaName").val();
-    submit(name);
+    submit.call(tmpl.data, name);
   },
 
 
   'click #savespecialArea': function (event, tmpl) {
     event.preventDefault();
     var name = $("#specialareaName").val();
-    console.log(name);
     submit.call(tmpl.data, name);
   }
 });
 
 function submit(name) {
   if (name) {
-    console.log(this.name);
-    //if (this.name == "general") {
-    //  Meteor.call("createGeneralArea", name.trim(), HospoHero.handleMethodResult(function () {
-    //    $("#generalareaName").val("");
-    //    $("#addNewGeneralAreaModal").modal("hide");
-    //  }));
-    //} else if (this.name == "special") {
-    //  var gareaId = Session.get("thisGeneralArea");
-    //  if (gareaId) {
-    //    Meteor.call("createSpecialArea", name, gareaId, HospoHero.handleMethodResult(function () {
-    //      $("#specialareaName").val("");
-    //      $("#addNewSpecialAreaModal").modal("hide")
-    //    }));
-    //  }
-    //}
+    if (this.name == "general") {
+      Meteor.call("createGeneralArea", name.trim(), HospoHero.handleMethodResult(function () {
+        $("#generalareaName").val("");
+        $("#addNewGeneralAreaModal").modal("hide");
+      }));
+    } else if (this.name == "special") {
+      var gareaId = Session.get("thisGeneralArea");
+      if (gareaId) {
+        Meteor.call("createSpecialArea", name, gareaId, HospoHero.handleMethodResult(function () {
+          $("#specialareaName").val("");
+          $("#addNewSpecialAreaModal").modal("hide")
+        }));
+      }
+    }
   }
 }
