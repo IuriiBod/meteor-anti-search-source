ff2e1b807f1f326b9c0771d56c2c4edb03479c55
Taras Dolynyuk <trsdln@gmail.com>
2015-12-11 02:05:43 +1100

    Move combo time editable into views

--- client/lib/components/alerts/messages/success/component.js
+++
@@ -1,7 +0,0 @@
-var component = FlowComponents.define("alertSuccess", function (props) {
-});
-
-component.state.isTemplateWeeklyRoster = function () {
-  var route = Router.current().route.getName();
-  return route == "templateWeeklyRoster";
-};
