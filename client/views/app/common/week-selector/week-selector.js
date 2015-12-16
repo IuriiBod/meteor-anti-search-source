8342cd90c5ddb33c58d762ad9b12d271f22405ff
alonoslav <vadym.vorobel@gmail.com>
2015-12-16 19:28:07 +1100

    Fixed template copying

--- client/views/app/common/week-selector/week-selector.js
+++ client/views/app/common/week-selector/week-selector.js
@@ -4,55 +4,70 @@ Template.weekSelector.rendered = function () {
   });
 };
 
-Template.weekSelector.events({
-  'click .saveShifts': function (event) {
-    event.preventDefault();
-    var weekNo = Session.get("templateToWeek");
-    var week = getDatesFromWeekNumber(weekNo);
-    var dates = [];
-    week.forEach(function (day) {
-      if (day && day.date) {
-        dates.push(new Date(day.date).getTime())
-      }
-    });
+Template.weekSelector.helpers({
+  futureWeeks: function () {
+    var currentMoment = moment().startOf('week');
+    var weeks = [];
 
-    week.forEach(function (obj) {
-      var index = HospoHero.dateUtils.shiftDate(moment().day(obj.day),true);
-      var shifts = Shifts.find({
-        startTime: TimeRangeQueryBuilder.forDay(index),
-        type: 'template'
-      }, {
-        sort: {order: 1}
-      }).fetch();
-
-      if (shifts.length > 0) {
-        shifts.forEach(function (shift) {
-          var startHour = moment(shift.startTime).hour();
-          var startMin = moment(shift.startTime).minute();
-
-          var endHour = moment(shift.endTime).hour();
-          var endMin = moment(shift.endTime).minute();
-
-          var info = {
-            "startTime": moment(obj.date).hour(startHour).minute(startMin).toDate(),
-            "endTime": moment(obj.date).hour(endHour).minute(endMin).toDate(),
-            "section": shift.section,
-            "assignedTo": shift.assignedTo,
-            "week": dates,
-            "order": shift.order
-          };
-
-          Meteor.call("createShift", info, HospoHero.handleMethodResult(function () {
-            $("#notifiModal").modal("show");
-          }));
-        });
-      }
-    });
+    for (var i=0; i<6; i++) {
+      weeks.push(
+        moment(currentMoment).toDate()
+      );
+      currentMoment.add(1, 'week');
+    }
+    return weeks;
   },
 
-  'click .checklist-content': function (event) {
-    var checked = $(event.target).is(":checked");
-    Session.set("templateToWeek", $(event.target).val());
-    Session.set("templateToYear", $(event.target).attr("data-year"));
+  weekFormat: function (date) {
+    return moment(date).week();
+  },
+  dateFormat: function (date) {
+    return moment(date).format("dddd, Do of MMMM YYYY");
+  }
+});
+
+Template.weekSelector.events({
+  'click .saveShifts': function (event, tmpl) {
+    event.preventDefault();
+
+    var selectedWeek = tmpl.get('selectedWeek');
+
+    if (selectedWeek) {
+      // new Date(selectedWeek) for overcoming moment js deprecated error
+      var templateSelectedWeek = moment(0).week(2).startOf('isoweek').day(moment(new Date(selectedWeek)).day());
+
+      var timeRangeForWeek = TimeRangeQueryBuilder.forWeek(templateSelectedWeek, HospoHero.getCurrentArea().locationId);
+      var shifts = Shifts.find({
+        startTime: timeRangeForWeek,
+        type: 'template'
+      });
+
+      var toCurrentDayMoment = function (date) {
+        var selectedWeekMoment = moment(new Date(selectedWeek));
+        return moment(date).set({
+          year: selectedWeekMoment.year(),
+          week: selectedWeekMoment.week()
+        }).toDate();
+      };
+
+      shifts.forEach(function (shift) {
+        delete shift._id;
+        shift.type = null;
+        shift.startTime = toCurrentDayMoment(shift.startTime);
+        shift.endTime = toCurrentDayMoment(shift.endTime);
+
+        Meteor.call("createShift", shift, HospoHero.handleMethodResult());
+      });
+
+      HospoHero.success('Template was copied to the week number ', moment(new Date(selectedWeek)).week());
+    } else {
+      HospoHero.error('You should select week at first');
+    }
+
+    return false;
+  },
+
+  'ifChecked [name=week-radio]': function (event, tmpl) {
+    tmpl.set('selectedWeek', event.target.value);
   }
 });
