ba6e1abd0defb1340f5e090f9570b4bf7d841ef1
alonoslav <vadym.vorobel@gmail.com>
2015-12-16 16:50:25 +0200

    Fixed basic details of supplier

--- client/views/app/suppliers/supplier-profile/basics/editable-define.js
+++ client/views/app/suppliers/supplier-profile/basics/editable-define.js
@@ -1,72 +1,8 @@
-Template.basics.onCreated(function () {
-  this.updateSupplierDetails = function (field, value) {
-    if (value) {
-      FlowComponents.callAction('updateSupplier', field, value);
-    }
-  };
-
-  this.triggerUpdateSupplier = function (field, value) {
-    this.data.field = field;
-    this.data.value = value;
-    $('.test-element').trigger('click');
-  }
-});
-
 Template.basics.onRendered(function () {
   defineEditableComponents(this);
 });
 
-Template.basics.helpers({
-  convertToPdfUrl: function () {
-    return '/convert?format=pdf';
-  }
-});
-
-Template.basics.events({
-  'click #uploadPriceList': function (e) {
-    e.preventDefault();
-    FlowComponents.callAction('uploadPriceList').then(function (res) {
-      if (res) {
-        $('.uploadedPriceList').removeClass('hide');
-        $('#uploadedImageUrl').attr('src', url);
-      }
-    });
-  },
-
-  'click .supplierStatus': function (e) {
-    e.preventDefault();
-    FlowComponents.callAction('changeSupplierStatus');
-  },
-
-  'click .delete-price-list': function (event) {
-    event.preventDefault();
-    FlowComponents.callAction('removePriceList', this);
-  },
-
-  'click .test-element': function (event, tmpl) {
-    if (tmpl.data.field) {
-      tmpl.updateSupplierDetails(tmpl.data.field, tmpl.data.value);
-    }
-  },
-
-  'change .delivery-day': function (event, tmpl) {
-    var supplierId = HospoHero.getParamsFromRoute(Router.current(), '_id');
-    var supplier = Suppliers.findOne({_id: supplierId});
-
-    var checkbox = event.target;
-    if (checkbox.checked) {
-      supplier.deliveryDays.push(checkbox.value);
-    } else {
-      var valueIndex = supplier.deliveryDays.indexOf(checkbox.value);
-      supplier.deliveryDays.splice(valueIndex, 1);
-    }
-    tmpl.updateSupplierDetails('deliveryDays', supplier.deliveryDays);
-  }
-});
-
-
 var defineEditableComponents = function (tmpl) {
-
   tmpl.$('.supplier-email').editable({
     type: 'text',
     title: 'Edit supplier email',
---
+++ client/views/app/suppliers/supplier-profile/basics/supplier-basics.js
@@ -0,0 +1,103 @@
+Template.basics.onCreated(function () {
+  this.supplier = function () {
+    return Suppliers.findOne({_id: this.data.id});
+  };
+
+  this.updateSupplierDetails = function (field, value) {
+    if (value) {
+      var supplier = this.supplier();
+      supplier[field] = value;
+      Meteor.call('editSupplier', supplier, HospoHero.handleMethodResult());
+    }
+  };
+
+  this.triggerUpdateSupplier = function (field, value) {
+    $('.test-element').trigger('updateSupplier', [field, value]);
+  }
+});
+
+
+Template.basics.helpers({
+  supplier: function () {
+    return Template.instance().supplier();
+  },
+  status: function () {
+    var supplier = Template.instance().supplier();
+    return supplier && supplier.active;
+  },
+  lastOrder: function () {
+    return OrderReceipts.findOne({'supplier': this.id}, {sort: {'date': -1}});
+  },
+  deliveryDays: function () {
+    return [
+      {value: 'monday', text: 'Monday'},
+      {value: 'tuesday', text: 'Tuesday'},
+      {value: 'wednesday', text: 'Wednesday'},
+      {value: 'thursday', text: 'Thursday'},
+      {value: 'friday', text: 'Friday'},
+      {value: 'saturday', text: 'Saturday'},
+      {value: 'sunday', text: 'Sunday'}
+    ];
+  },
+  isSelectedDay: function (value) {
+    var supplier = Template.instance().supplier();
+    return supplier && supplier.deliveryDays.indexOf(value) > -1;
+  },
+  convertToPdfUrl: function () {
+    return '/convert?format=pdf';
+  }
+});
+
+Template.basics.events({
+  'click #uploadPriceList': function (e, tmpl) {
+    e.preventDefault();
+    filepicker.pickMultiple({
+      services: ['COMPUTER']
+    }, function (blobs) {
+      if (_.isArray(blobs)) {
+        var urlArray = _.map(blobs, function (doc) {
+          return {
+            url: doc.url,
+            name: doc.filename,
+            uploadedAt: new Date()
+          };
+        });
+        Meteor.call('addPriceList', tmpl.data.id, urlArray, HospoHero.handleMethodResult());
+      }
+    }, HospoHero.handleMethodResult(function () {
+      $('.uploadedPriceList').removeClass('hide');
+      $('#uploadedImageUrl').attr('src', url);
+    }));
+  },
+
+  'click .supplierStatus': function (e, tmpl) {
+    e.preventDefault();
+    Meteor.call('activateReactivateSuppliers', tmpl.data.id, HospoHero.handleMethodResult(function () {
+      Router.go('suppliersList');
+    }));
+  },
+
+  'click .delete-price-list': function (event, tmpl) {
+    event.preventDefault();
+    Meteor.call('removePriceList', tmpl.data.id, this, HospoHero.handleMethodResult());
+  },
+
+  'updateSupplier .test-element': function (event, tmpl, field, value) {
+    if (field) {
+      tmpl.updateSupplierDetails(field, value);
+    }
+  },
+
+  'change .delivery-day': function (event, tmpl) {
+    var supplier = tmpl.supplier();
+
+    var checkbox = event.target;
+    if (checkbox.checked) {
+      supplier.deliveryDays.push(checkbox.value);
+    } else {
+      var valueIndex = supplier.deliveryDays.indexOf(checkbox.value);
+      supplier.deliveryDays.splice(valueIndex, 1);
+    }
+    tmpl.updateSupplierDetails('deliveryDays', supplier.deliveryDays);
+  }
+});
