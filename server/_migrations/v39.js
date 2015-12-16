e9f067dbe9a6637e91b8d331e63aa595272918e2
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-12-16 11:44:24 +0200

    Improve migration

--- server/_migrations/v39.js
+++ server/_migrations/v39.js
@@ -3,15 +3,61 @@ Migrations.add({
   name: "Change property name in list of ingredients in jobItemsSubmitEdit",
   up: function () {
     JobItems.find().forEach(function (jobItem) {
-      var ingredients = _.map(jobItem.ingredients, function (ingredient) {
-        return {
-          id: ingredient._id,
-          quantity: ingredient.quantity
-        }
-      });
-      if (ingredients) {
-        JobItems.update({_id: jobItem._id}, {$set: {ingredients: ingredients}});
+      if (jobItem.ingredients) {
+        var ingredients = _.map(jobItem.ingredients, function (ingredient) {
+          return {
+            id: ingredient._id,
+            quantity: ingredient.quantity
+          }
+        });
+        jobItem.ingredients = ingredients;
       }
+
+      if (jobItem.startsOn) {
+        var startsOn = new Date(jobItem.startsOn);
+        jobItem.startsOn = startsOn;
+      }
+
+      if (jobItem.repeatAt) {
+        var repeatAt = new Date(jobItem.repeatAt);
+        jobItem.repeatAt = repeatAt;
+      }
+
+      if (jobItem.createdOn) {
+        var createdOn = new Date(jobItem.createdOn);
+        jobItem.createdOn = createdOn;
+      }
+
+      if (jobItem.editedOn) {
+        var editedOn = new Date(jobItem.editedOn);
+        jobItem.editedOn = editedOn;
+      }
+
+      if (jobItem.endsOn && jobItem.endsOn.lastDate) {
+        var lastDate = new Date(jobItem.endsOn.lastDate);
+        jobItem.endsOn.lastDate = lastDate;
+      }
+
+      if (jobItem.frequency) {
+        var frequency = jobItem.frequency;
+        switch (frequency) {
+          case 'Daily':
+            jobItem.frequency = 'daily';
+            break;
+          case 'Weekly':
+            jobItem.frequency = 'weekly';
+            break;
+          case 'Every X Weeks':
+            jobItem.frequency = 'everyXWeeks';
+        }
+      }
+
+      if (jobItem.step) {
+        jobItem.repeatEvery = jobItem.step;
+        delete jobItem.step;
+      }
+
+      JobItems.update({_id: jobItem._id}, jobItem);
     });
   }
 });
