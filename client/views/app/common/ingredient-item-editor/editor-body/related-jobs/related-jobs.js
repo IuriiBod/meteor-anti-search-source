19e090ad22921b8fa80b2e50bf3376671cb3d2f7
Taras Dolynyuk <trsdln@gmail.com>
2015-12-17 13:39:13 +0200

    Finished related jobs view

--- client/views/app/common/ingredient-item-editor/editor-body/related-jobs/related-jobs.js
+++ client/views/app/common/ingredient-item-editor/editor-body/related-jobs/related-jobs.js
@@ -2,5 +2,10 @@
 Template.ingredientEditorRelatedJobs.helpers({
   relatedJobs: function () {
     return JobItems.find({"ingredients._id": this._id});
+  },
+
+  secondsToMinutes: function (secondsCount) {
+    var duration = moment.duration(secondsCount, 'seconds');
+    return duration.minutes() + ':' + duration.seconds();
   }
 });
