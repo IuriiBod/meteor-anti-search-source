60c48c29e72c2ca63e65be4021ad3b27623e7144
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-12-18 17:30:29 +0200

    Using anti-ss in jobItemsList

--- client/views/app/job-items/master-list/job-items-master-list.js
+++ client/views/app/job-items/master-list/job-items-master-list.js
@@ -3,14 +3,31 @@ Template.jobItemsList.onCreated(function () {
     collection: 'jobItems',
     fields: ['name', 'recipe', 'description'],
     searchMode: 'local',
-    mongoQuery: {
-      status: this.data.status,
-      type: JobTypes.findOne({name: this.data.type})._id
-    },
     limit: 10
   });
 
+  // when jop type changes, set new mongo query
+  var self = this;
+  this.autorun(function () {
+    var type = Template.currentData().type;
+    var typeId = JobTypes.findOne({name: type})._id;
+    self.searchSource.setMongoQuery({type: typeId});
+  });
   this.searchSource.search('');
+});
+
+Template.jobItemsList.onRendered(function () {
+  // for infinite scroll
+  var tmpl = this;
+  $('#wrapper').scroll(function (e) {
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+
+    if ((docHeight - winHeight) == scrollTop) {
+      tmpl.$('.load-more-items').click();
+    }
+  });
 });
 
 Template.jobItemsList.helpers({
@@ -26,5 +43,9 @@ Template.jobItemsList.events({
   'keyup .search-job-items-box': _.throttle(function (e, tmpl) {
     var value = $(e.target).val();
     tmpl.searchSource.search(value);
-  }, 500)
+  }, 500),
+
+  'click .load-more-items': function (e, tmpl) {
+    tmpl.searchSource.incrementLimit(10);
+  }
 });
