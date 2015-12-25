d59738b18c12c65653b64ce6a783ea7046fe7ea6
alonoslav <vadym.vorobel@gmail.com>
2015-12-25 15:21:55 +0200

    Fixed all ingredientItemEditor

--- client/views/app/stock/list-view.js
+++ client/views/app/stock/list-view.js
@@ -1,6 +1,4 @@
 Template.listOfStocksMasterMainView.onCreated(function () {
-  this.set('ingredientItemEditorExist', null);
-
   this.onIngredientIdChange = function () {
     var self = this;
 
@@ -8,19 +6,12 @@ Template.listOfStocksMasterMainView.onCreated(function () {
       if (ingredient) {
         ingredient = Ingredients.findOne({_id: ingredient});
       }
-
       self.ingredientItemEditorModal = ModalManager.open('ingredientItemEditor', {ingredient: ingredient});
     }
   }
 });
 
 Template.listOfStocksMasterMainView.helpers({
-  templateData: function () {
-    return {
-      onIngredientIdChange: Template.instance().onIngredientIdChange()
-    }
-  },
-
   onIngredientIdChange: function () {
     return Template.instance().onIngredientIdChange();
   }
--- client/views/app/suppliers/supplier-profile/products/procucts-list.js
+++ client/views/app/suppliers/supplier-profile/products/procucts-list.js
@@ -1,7 +1,3 @@
-Template.productsList.onCreated(function () {
-  this.set('ingredientItemEditorExist', false);
-});
-
 Template.productsList.helpers({
   products: function () {
     return Ingredients.find({suppliers: this.id}, {fields: {_id: 1}});
@@ -11,19 +7,7 @@ Template.productsList.helpers({
 Template.productsList.events({
   'click .editProduct': function (event, tmpl) {
     event.preventDefault();
-
-    if (tmpl.get('ingredientItemEditorExist')) {
-      Blaze.remove(tmpl.get('ingredientItemEditorExist'));
-    }
-
     var ingredient = Ingredients.findOne({_id: this.ingredientId});
-
-    var ingredientEditorBlazeView = Blaze.renderWithData(Template.ingredientItemEditor, {
-      isModal: true,
-      ingredient: ingredient
-    }, document.getElementById('ingredientItemEditorPlaceHolder'));
-
-    tmpl.set('ingredientItemEditorExist', ingredientEditorBlazeView);
-    tmpl.$("#ingredientItemEditor").modal("show");
+    tmpl.ingredientItemEditorModal = ModalManager.open('ingredientItemEditor', {ingredient: ingredient});
   }
 });
