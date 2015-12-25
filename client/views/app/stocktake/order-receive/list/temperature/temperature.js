8d14942eb08ffd1fb073b639b27ac86d959dc92a
Bohdan Semynovich <bodya.ozzy@gmail.com>
2015-12-25 16:48:14 +0200

    Another one small-refactoring in temperature

--- client/views/app/stocktake/order-receive/list/temperature/temperature.js
+++ client/views/app/stocktake/order-receive/list/temperature/temperature.js
@@ -1,8 +1,9 @@
 Template.temperatureModal.events({
   'submit .temperature-form': function (event, tmpl) {
     event.preventDefault();
+    var $form  =$(event.target);
+    var temp = $form.find('.temperature-input').val();
     var receiptId = tmpl.data.currentReceipt._id;
-    var temp = $(event.target).find('temperature-input').val();
     if (temp) {
       var info = {'temperature': temp};
       Meteor.call('updateReceipt', receiptId, info, HospoHero.handleMethodResult(function () {
