b98475b3ed8e749d161621b6aaad3fedbf477c90
ahio <stepan.te.ua@gmail.com>
2015-12-25 13:08:43 +0200

    refactored stocktake header

---
+++ client/views/app/stocktake/stocktake-header/stocktake-header.js
@@ -0,0 +1,18 @@
+Template.stocktakeHeader.events({
+  'click #startNewStocktake': function (event, tmpl) {
+    event.preventDefault();
+    var date = moment().format("YYYY-MM-DD");
+    var stocktake = StocktakeMain.findOne({
+      "stocktakeDate": new Date(date).getTime(),
+      "relations.areaId": HospoHero.getCurrentAreaId()
+    });
+
+    if (stocktake) {
+      tmpl.$("#newStocktakeModal").modal();
+    } else {
+      Meteor.call("createMainStocktake", date, HospoHero.handleMethodResult(function (id) {
+        Router.go("stocktakeCounting", {"_id": id});
+      }));
+    }
+  }
+});
