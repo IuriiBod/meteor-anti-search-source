7d8c62bcbfb66afb82288e34848bf4ecdb4c4784
ahio <stepan.te.ua@gmail.com>
2016-01-04 16:08:40 +0200

    migration that changes timestamps to date

---
+++ server/_migrations/v45.js
@@ -0,0 +1,65 @@
+Migrations.add({
+  version: 45,
+  name: "changed timestamps to date in collections",
+  up: function () {
+    OrderReceipts.find().forEach(function (order) {
+      if (order.date && _.isString(order.date)) {
+        OrderReceipts.update({_id: order._id}, {
+          $set: {
+            date: new Date(Number(order.date))
+          }
+        });
+      } else if (order.date && _.isNumber(order.date)) {
+        OrderReceipts.update({_id: order._id}, {
+          $set: {
+            date: new Date(order.date)
+          }
+        });
+      }
+
+      if (order.expectedDeliveryDate && _.isString(order.expectedDeliveryDate)) {
+        OrderReceipts.update({_id: order._id}, {
+          $set: {
+            expectedDeliveryDate: new Date(Number(order.expectedDeliveryDate))
+          }
+        });
+      } else if (order.expectedDeliveryDate && _.isNumber(order.expectedDeliveryDate)) {
+        OrderReceipts.update({_id: order._id}, {
+          $set: {
+            expectedDeliveryDate: new Date(order.expectedDeliveryDate)
+          }
+        });
+      }
+    });
+
+    StockOrders.find().forEach(function(item) {
+      if (item.orderedOn && _.isString(item.orderedOn)) {
+        StockOrders.update({_id: item._id}, {
+          $set: {
+            orderedOn: new Date(Number(item.orderedOn))
+          }
+        });
+      } else if (item.orderedOn && _.isNumber(item.orderedOn)) {
+        StockOrders.update({_id: item._id}, {
+          $set: {
+            orderedOn: new Date(item.orderedOn)
+          }
+        });
+      }
+
+      if (item.expectedDeliveryDate && _.isString(item.expectedDeliveryDate)) {
+        StockOrders.update({_id: item._id}, {
+          $set: {
+            expectedDeliveryDate: new Date(Number(item.expectedDeliveryDate))
+          }
+        });
+      } else if (item.expectedDeliveryDate && _.isNumber(item.expectedDeliveryDate)) {
+        StockOrders.update({_id: item._id}, {
+          $set: {
+            expectedDeliveryDate: new Date(item.expectedDeliveryDate)
+          }
+        });
+      }
+    });
+  }
+});
