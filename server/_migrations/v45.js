4c1d2ae210bc469f947033907df6e1f94bdf46bc
ahio <stepan.te.ua@gmail.com>
2016-01-04 16:21:11 +0200

    code styling...

--- server/_migrations/v45.js
+++ server/_migrations/v45.js
@@ -4,13 +4,17 @@ Migrations.add({
   up: function () {
     OrderReceipts.find().forEach(function (order) {
       if (order.date && _.isString(order.date)) {
-        OrderReceipts.update({_id: order._id}, {
+        OrderReceipts.update({
+          _id: order._id
+        }, {
           $set: {
             date: new Date(Number(order.date))
           }
         });
       } else if (order.date && _.isNumber(order.date)) {
-        OrderReceipts.update({_id: order._id}, {
+        OrderReceipts.update({
+          _id: order._id
+        }, {
           $set: {
             date: new Date(order.date)
           }
@@ -18,13 +22,17 @@ Migrations.add({
       }
 
       if (order.expectedDeliveryDate && _.isString(order.expectedDeliveryDate)) {
-        OrderReceipts.update({_id: order._id}, {
+        OrderReceipts.update({
+          _id: order._id
+        }, {
           $set: {
             expectedDeliveryDate: new Date(Number(order.expectedDeliveryDate))
           }
         });
       } else if (order.expectedDeliveryDate && _.isNumber(order.expectedDeliveryDate)) {
-        OrderReceipts.update({_id: order._id}, {
+        OrderReceipts.update({
+          _id: order._id
+        }, {
           $set: {
             expectedDeliveryDate: new Date(order.expectedDeliveryDate)
           }
@@ -32,15 +40,19 @@ Migrations.add({
       }
     });
 
-    StockOrders.find().forEach(function(item) {
+    StockOrders.find().forEach(function (item) {
       if (item.orderedOn && _.isString(item.orderedOn)) {
-        StockOrders.update({_id: item._id}, {
+        StockOrders.update({
+          _id: item._id
+        }, {
           $set: {
             orderedOn: new Date(Number(item.orderedOn))
           }
         });
       } else if (item.orderedOn && _.isNumber(item.orderedOn)) {
-        StockOrders.update({_id: item._id}, {
+        StockOrders.update({
+          _id: item._id
+        }, {
           $set: {
             orderedOn: new Date(item.orderedOn)
           }
@@ -48,13 +60,17 @@ Migrations.add({
       }
 
       if (item.expectedDeliveryDate && _.isString(item.expectedDeliveryDate)) {
-        StockOrders.update({_id: item._id}, {
+        StockOrders.update({
+          _id: item._id
+        }, {
           $set: {
             expectedDeliveryDate: new Date(Number(item.expectedDeliveryDate))
           }
         });
       } else if (item.expectedDeliveryDate && _.isNumber(item.expectedDeliveryDate)) {
-        StockOrders.update({_id: item._id}, {
+        StockOrders.update({
+          _id: item._id
+        }, {
           $set: {
             expectedDeliveryDate: new Date(item.expectedDeliveryDate)
           }
