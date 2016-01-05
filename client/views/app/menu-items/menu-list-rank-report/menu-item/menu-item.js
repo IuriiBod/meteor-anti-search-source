04b82d8efa95a472d413d6fcee70d39a7a282993
ahio <stepan.te.ua@gmail.com>
2016-01-05 15:38:23 +0200

    total price from sales

--- client/views/app/menu-items/menu-list-rank-report/menu-item/menu-item.js
+++ client/views/app/menu-items/menu-list-rank-report/menu-item/menu-item.js
@@ -1,8 +1,17 @@
+Template.menuItemReport.onCreated(function() {
+  this.getItemSalesQuantity = function() {
+    return DailySales.findOne({menuItemId: this.data.item._id});
+  };
+});
+
 Template.menuItemReport.helpers({
-  itemSalesQuantity: function(itemId) {
-    var menuItemDailySales = DailySales.findOne({menuItemId: itemId});
+  itemSalesQuantity: function() {
+    var menuItemDailySales = Template.instance().getItemSalesQuantity();
     return menuItemDailySales && menuItemDailySales.actualQuantity || 0;
   },
-
+  itemTotalPriceFromSales: function() {
+    var menuItemDailySales = Template.instance().getItemSalesQuantity();
+    return menuItemDailySales && menuItemDailySales.actualQuantity * this.item.salesPrice || 0;
+  },
 
 });
