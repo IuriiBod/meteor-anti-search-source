54ce331fd19ee6e411e0e1e907b8bda6ea742ccc
alonoslav <vadym.vorobel@gmail.com>
2016-01-13 10:11:32 +0200

    fixed timezone publication

--- server/organizations/publications.js
+++ server/organizations/publications.js
@@ -101,6 +101,7 @@ Meteor.publishComposite('organizationInfo', {
             if (!HospoHero.canUser('edit locations', this.userId)) {
               fields.name = 1;
               fields.organizationId = 1;
+              fields.timezone = 1;
             }
           }
           return Locations.find(query, {fields: fields});
