ab218f73e4ee8eb20cdd75d99cb35fa2c74a8877
Taras Dolynyuk <trsdln@gmail.com>
2016-01-20 16:31:23 +0200

    Improved date formating in roster table

--- client/views/app/roster/weekly-figures/roster-table/roster-table.js
+++ client/views/app/roster/weekly-figures/roster-table/roster-table.js
@@ -1,5 +1,8 @@
 Template.rosterTable.helpers({
   weekDate: function () {
     return HospoHero.getParamsFromRoute('date');
+  },
+  rosterTableDateFormat: function (date) {
+    return HospoHero.dateUtils.shortDateFormat(moment(date));
   }
 });
