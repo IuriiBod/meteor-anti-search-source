02d03b0be26abc0e95a2f5d2b27e2bb6e9fc6c4d
Vlad Golubev <golubev.vld@gmail.com>
2016-01-29 19:14:56 +0200

    Implement OrdersReporter

--- server/reports/reporters/orders-reporter.js
+++ server/reports/reporters/orders-reporter.js
@@ -6,10 +6,47 @@ OrdersReporter = class {
   constructor(fromDate, toDate) {
     this._fromDate = fromDate;
     this._toDate = toDate;
-    this._dateQuery = TimeRangeQueryBuilder.forInterval(moment(fromDate, 'DD/MM/YY'), moment(toDate, 'DD/MM/YY'));
+    this._dateQuery = {
+      $gte: moment(fromDate, 'DD/MM/YY').unix() * 1000, // TODO Change in case of migration to plain Date object in collection
+      $lte: moment(toDate, 'DD/MM/YY').unix() * 1000
+    };
   }
 
   getTotalOrdersReceived() {
-    return 350;
+    let totalOrdersReceived = _.reduce(this._getStockOrders(), (memo, value) => {
+      return memo + (value.unitPrice * value.countOrdered);
+    }, 0);
+    return HospoHero.misc.rounding(totalOrdersReceived, 100);
+  }
+
+  _getStockOrders() {
+    let orderReceiptsIds = this._getOrderReceiptsIds();
+    let findQuery = {
+      orderReceipt: {
+        $in: orderReceiptsIds
+      }
+    };
+    let projectionQuery = {
+      fields: {
+        _id: 0,
+        unitPrice: 1,
+        countOrdered: 1
+      }
+    };
+
+    return StockOrders.find(findQuery, projectionQuery).fetch();
+  }
+
+  _getOrderReceiptsIds() {
+    let findQuery = {
+      stocktakeDate: this._dateQuery
+    };
+    let projectionQuery = {
+      fields: {
+        _id: 1
+      }
+    };
+
+    return _.pluck(OrderReceipts.find(findQuery, projectionQuery).fetch(), '_id');
   }
 };
