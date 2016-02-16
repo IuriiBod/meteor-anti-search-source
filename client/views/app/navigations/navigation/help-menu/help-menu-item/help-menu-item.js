f2a558a640e3db9b03091969eaad8299919df3b9
v.danilichev <v.danilichev@gmail.com>
2016-02-16 18:40:19 +0200

    Added help menu entries

--- client/views/app/navigations/navigation/help-menu/help-menu-item/help-menu-item.js
+++
@@ -1,7 +0,0 @@
-Template.helpMenuItem.events({
-  'click .open-module': function(event) {
-    event.preventDefault();
-    var moduleName = event.currentTarget.getAttribute('data-elevio-module-name');
-    window._elev.openModule(moduleName);
-  }
-});
