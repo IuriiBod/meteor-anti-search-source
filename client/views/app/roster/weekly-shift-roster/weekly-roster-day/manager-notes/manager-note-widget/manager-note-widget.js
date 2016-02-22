f65c369aacf1443f42bbe62ec67234d1c0181bfe
v.danilichev <v.danilichev@gmail.com>
2016-02-22 13:51:33 +0200

    Added autosaveTextEditor

--- client/views/app/roster/weekly-shift-roster/weekly-roster-day/manager-notes/manager-note-widget/manager-note-widget.js
+++ client/views/app/roster/weekly-shift-roster/weekly-roster-day/manager-notes/manager-note-widget/manager-note-widget.js
@@ -1,84 +1,48 @@
 //Context: date (Date)
 
 Template.managerNoteWidget.onCreated(function () {
-  var self = this;
-  var commentsSubscription;
+  let self = this;
 
   self.note = new ReactiveVar();
 
   self.subscribe('managerNote', self.data.date, HospoHero.getCurrentAreaId(), function onReady () {
-    self.autorun(function () {
-      var note = ManagerNotes.findOne({
+    self.autorun(() => {
+      let note = ManagerNotes.findOne({
         noteDate: self.data.date,
         'relations.areaId': HospoHero.getCurrentAreaId()
       });
 
       if (note) {
         self.note.set(note);
-        commentsSubscription = Meteor.subscribe('comments', note._id, HospoHero.getCurrentAreaId());
-      } else if (commentsSubscription) {
-        self.note.set(null);
-        commentsSubscription.stop();
+      } else {
+        self.note.set({text: 'Leave your note here'});
       }
     });
   });
-
-  self.isTextOfNoteChanged = new ReactiveVar(false);
 });
 
 Template.managerNoteWidget.helpers({
-  note: function () {
+  note () {
     return Template.instance().note.get();
   },
-  isTextOfNoteChanged: function () {
-    return Template.instance().isTextOfNoteChanged.get();
-  }
-});
+  saveNote () {
+    let tmpl = Template.instance();
 
-Template.managerNoteWidget.events({
-  'keyup textarea[name=note-text]': function (event, tmpl) {
-    var note = tmpl.note.get() || {text: ''};
+    return (text) => {
+      let note = tmpl.note.get();
+      note.text = text;
 
-    if (note.text !== event.target.value) {
-      tmpl.isTextOfNoteChanged.set(true);
-    } else {
-      tmpl.isTextOfNoteChanged.set(false);
+      if (!note._id) {
+        note.noteDate = tmpl.data.date;
+        note.createdAt = new Date();
+        note.createdBy = Meteor.userId();
+        note.relations = HospoHero.getRelationsObject();
+      }
+
+      Meteor.call('upsertManagerNote', note);
     }
   },
-  'submit form': function(event, tmpl) {
-    event.preventDefault();
-
-    var text = tmpl.$('textarea[name=note-text]').val();
-    if (!text) return;
-
-    var note = tmpl.note.get() || {};
-    note.text = text;
-
-    if (!note._id) {
-      note.noteDate = tmpl.data.date;
-      note.createdAt = new Date();
-      note.createdBy = Meteor.userId();
-      note.relations = HospoHero.getRelationsObject();
-    }
-
-    Meteor.call('upsertManagerNote', note, HospoHero.handleMethodResult(function () {
-      tmpl.isTextOfNoteChanged.set(false);
-    }));
-  },
-  'click .cancel-changes': function (event, tmpl) {
-    event.preventDefault();
-
-    var note = tmpl.note.get() || {text: ''};
-
-    tmpl.$('textarea[name=note-text]').val(note.text);
-    tmpl.isTextOfNoteChanged.set(false);
-  },
-  'click .remove-note': function (event, tmpl) {
-    event.preventDefault();
-
-    var note = tmpl.note.get();
-    if (!note) return;
-
-    Meteor.call('deleteManagerNote', note._id);
+  readOnly () {
+    return !HospoHero.canUser('edit roster', Meteor.userId());
   }
 });
