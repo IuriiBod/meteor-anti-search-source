82dc46db6dbb9c3d3ec53f1b8608dc00105cecc2
Taras Dolynyuk <trsdln@gmail.com>
2016-03-02 12:51:24 +0200

    Remove redudant migrations

--- server/_migrations/v60.js
+++
@@ -1,9 +0,0 @@
-Migrations.add({
-  version: 60,
-  name: 'remove invitation collection',
-  up: function () {
-    let collectionName = 'invitations';
-    Migrations.utils.removeCollection(collectionName);
-    console.log('Goodbye Invitations!');
-  }
-});
