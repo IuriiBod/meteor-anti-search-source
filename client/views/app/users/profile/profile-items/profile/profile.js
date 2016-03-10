5b812552645d3bfc5e13e3cfa6d596c1da0d89cd
vova tymets <volodymyrtymets@gmail.com>
2016-03-10 10:50:15 +0200

    add password change functional

--- client/views/app/users/profile/profile-items/profile/profile.js
+++ client/views/app/users/profile/profile-items/profile/profile.js
@@ -101,11 +101,34 @@ Template.profile.events({
     Meteor.call("resignDate", "remove", id, '', HospoHero.handleMethodResult());
   },
 
-  "submit form#change-pin": function (event) {
+  'change input#confirm-password': function (e,t) {
+    var $password = t.find("#new-password");
+    var $confirm = t.find("#confirm-password");
+
+    if($password.value !== $confirm.value) {
+      $confirm.setCustomValidity("Passwords Don't Match");
+    } else {
+      $confirm.setCustomValidity('');
+    }
+  },
+
+  'submit form#change-pin': function (event) {
     event.preventDefault();
     var newPin = Template.instance().find("#new-pin").value;
     Meteor.call("changePinCode", newPin, HospoHero.handleMethodResult(function () {
       HospoHero.info("PIN has been changed");
+    }));
+  },
+
+  'submit form#change-password': function (event,t) {
+    event.preventDefault();
+    var $oldPassword = t.find("#old-password");
+    var $newPassword = t.find("#new-password");
+
+    if(!$(event.target)[0].checkValidity()) return;
+
+    Accounts.changePassword($oldPassword.value, $newPassword.value, HospoHero.handleMethodResult(function () {
+        HospoHero.info("Password has been changed");
     }));
   }
 });
