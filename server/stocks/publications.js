3c5bddd5c3e126b8f653de9cf497de12f11c0378
ahio <stepan.te.ua@gmail.com>
2016-03-15 11:32:30 +0200

    refactored publications

--- server/stocks/publications.js
+++ server/stocks/publications.js
@@ -1,21 +1,19 @@
 Meteor.publish('allIngredientsInArea', function (areaId, status) {
-  if (this.userId) {
-    check(areaId, HospoHero.checkers.MongoId);
-    check(status, Match.OneOf(String, null));
+  check(areaId, HospoHero.checkers.MongoId);
+  check(status, Match.OneOf(String, null));
 
-    let checkPermission = new HospoHero.security.PermissionChecker(this.userId);
+  let checkPermission = this.userId && new HospoHero.security.PermissionChecker(this.userId);
 
-    if (checkPermission.hasPermissionInArea(areaId, "edit stocks")) {
-      var query = {
-        'relations.areaId': areaId
-      };
+  if (checkPermission && checkPermission.hasPermissionInArea(areaId, "edit stocks")) {
+    var query = {
+      'relations.areaId': areaId
+    };
 
-      if (status) {
-        query.status = status;
-      }
-
-      return Ingredients.find(query);
+    if (status) {
+      query.status = status;
     }
+
+    return Ingredients.find(query);
   } else {
     this.ready();
   }
@@ -31,18 +29,14 @@ Meteor.publish('ingredientsRelatedJobs', function (id) {
 });
 
 Meteor.publish('ingredient', function (id) {
-  if (this.userId) {
-    check(id, HospoHero.checkers.MongoId);
+  check(id, HospoHero.checkers.MongoId);
 
-    let ingredientCursor = Ingredients.find({_id: id});
-    if (ingredientCursor.count()) {
-      let ingredientAreaId = ingredientCursor.fetch()[0].relations.areaId;
-      let checkPermission = new HospoHero.security.PermissionChecker(this.userId);
+  let checkPermission = this.userId && new HospoHero.security.PermissionChecker(this.userId);
+  let ingredientCursor = Ingredients.find({_id: id});
+  let ingredientAreaId = ingredientCursor.count() && ingredientCursor.fetch()[0].relations.areaId;
 
-      if (checkPermission.hasPermissionInArea(ingredientAreaId, "edit stocks")) {
-        return ingredientCursor;
-      }
-    }
+  if (checkPermission && ingredientAreaId && checkPermission.hasPermissionInArea(ingredientAreaId, "edit stocks")) {
+    return ingredientCursor;
   } else {
     this.ready();
   }
