fc940abe1ae5a3fa2925b95c69ab1d0510ce5713
ahio <stepan.te.ua@gmail.com>
2016-03-17 02:14:58 +1100

    changed way of removing shifts with wrong dates, because of a lot different wrong dates

--- server/_migrations/v65.js
+++ server/_migrations/v65.js
@@ -1,27 +1,15 @@
 Migrations.add({
   version: 65,
-  name: 'Remove shifts with wrong date (shifts with wrong date not shown in template and cause error)',
+  name: 'Remove shifts with wrong date. Shifts with wrong date not shown in template and cause error',
   up: function () {
-    var removeShifts = function (date) {
-      var shiftsCursor = Shifts.find({startTime: date});
-      if (shiftsCursor.count()) {
-        shiftsCursor.forEach(function (shift) {
-          Shifts.remove({_id: shift._id});
-        });
-      }
-    };
+    var dateRange = TimeRangeQueryBuilder.forWeek(moment(0).week(2).startOf('isoweek'));
+    var copiedShifts = Shifts.find({type: 'template', startTime: dateRange}).fetch();
 
-    var customDateRange = {
-      $gte: new Date('Tue Dec 01 2015 00:00:00'),
-      $lte: new Date('Thu Dec 31 2015 23:59:59')
-    };
+    Shifts.remove({type: 'template'});
 
-    removeShifts(customDateRange);
+    copiedShifts.forEach(function (shift) {
+      Shifts.insert(shift);
+    });
 
-    for (var i = 0; i < 4; i++) {
-      var day = TimeRangeQueryBuilder.forDay(moment(0).add(i, 'day'));
-
-      removeShifts(day);
-    }
   }
 });
