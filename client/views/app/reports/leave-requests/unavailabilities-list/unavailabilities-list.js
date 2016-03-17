32e57e10c577b0a3f67d7d5e0119df7ef8207c45
vova tymets <volodymyrtymets@gmail.com>
2016-03-17 09:48:10 +0200

    replace tab po space

--- client/views/app/reports/leave-requests/unavailabilities-list/unavailabilities-list.js
+++ client/views/app/reports/leave-requests/unavailabilities-list/unavailabilities-list.js
@@ -1,27 +1,27 @@
 Template.unavailabilitiesList.onCreated(function () {
-	this.page = new Blaze.ReactiveVar(1);
-	this.query = {'unavailabilities.0': {'$exists': true}};
-	const currentAreaId = HospoHero.getCurrentAreaId(Meteor.userId());
-	this.autorun(() => {
-		this.subscribe('areaUnavailabilitiesList', currentAreaId,
-			this.page.get() * this.data.temPerPage);
-	});
+  this.page = new Blaze.ReactiveVar(1);
+  this.query = {'unavailabilities.0': {'$exists': true}};
+  const currentAreaId = HospoHero.getCurrentAreaId(Meteor.userId());
+  this.autorun(() => {
+    this.subscribe('areaUnavailabilitiesList', currentAreaId,
+      this.page.get() * this.data.temPerPage);
+  });
 });
 
 Template.unavailabilitiesList.helpers({
-	users: function () {
-		return Meteor.users.find(Template.instance().query);
-	},
-	isHasMoreItems: function () {
-		let page = Template.instance().page.get();
-		return Meteor.users.find(Template.instance().query).count() /
-			(page * this.itemPerPage) >= 1;
-	}
+  users: function () {
+    return Meteor.users.find(Template.instance().query);
+  },
+  isHasMoreItems: function () {
+    let page = Template.instance().page.get();
+    return Meteor.users.find(Template.instance().query).count() /
+      (page * this.itemPerPage) >= 1;
+  }
 });
 Template.unavailabilitiesList.events({
-	'click [data-action="load-more"]': (e, t) => {
-		e.preventDefault();
-		var page = t.page.get();
-		t.page.set(page + 1);
-	}
+  'click [data-action="load-more"]': (e, t) => {
+    e.preventDefault();
+    var page = t.page.get();
+    t.page.set(page + 1);
+  }
 });
