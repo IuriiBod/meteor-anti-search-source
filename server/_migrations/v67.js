0ea424a49d202e5130e72dadc829e7fff067fcad
ahio <stepan.te.ua@gmail.com>
2016-03-17 19:23:42 +0200

    fixed migration

--- server/_migrations/v67.js
+++ server/_migrations/v67.js
@@ -3,12 +3,19 @@ Migrations.add({
   name: 'Remove shifts with wrong date. Shifts with wrong date not shown in template and cause error',
   up: function () {
     var dateRange = TimeRangeQueryBuilder.forWeek(moment(0).week(2).startOf('isoweek'));
-    var copiedShifts = Shifts.find({type: 'template', startTime: dateRange}).fetch();
-
-    Shifts.remove({type: 'template'});
-
-    copiedShifts.forEach(function (shift) {
-      Shifts.insert(shift);
+    Shifts.remove({
+      type: 'template',
+      $or: [
+        {
+          startTime: {
+            $lt: dateRange.$gte
+          }
+        }, {
+          endTime: {
+            $gt: dateRange.$lte
+          }
+        }
+      ]
     });
 
   }
