38e49bea2e9a720fc38c250188848b32702a87e5
vova tymets <volodymyrtymets@gmail.com>
2016-03-17 09:45:37 +0200

    replace tab po space

--- client/views/app/reports/leave-requests/leave-requests-list/leave-requests-list.js
+++ client/views/app/reports/leave-requests/leave-requests-list/leave-requests-list.js
@@ -1,37 +1,37 @@
 Template.leaveRequestsList.onCreated(function () {
-	this.page = new Blaze.ReactiveVar(1);
-	const currentAreaId = HospoHero.getCurrentAreaId(Meteor.userId());
-	this.autorun(() => {
-		this.subscribe('leaveRequests', currentAreaId,
-			this.page.get() * this.data.itemPerPage);
-	});
+  this.page = new Blaze.ReactiveVar(1);
+  const currentAreaId = HospoHero.getCurrentAreaId(Meteor.userId());
+  this.autorun(() => {
+    this.subscribe('leaveRequests', currentAreaId,
+      this.page.get() * this.data.itemPerPage);
+  });
 });
 
 Template.leaveRequestsList.helpers({
-	leaveRequestsItems: function () {
-		return LeaveRequests.find();
-	},
-	date: function () {
-		return arguments[0].toLocaleDateString();
-	},
-	username: function () {
-		var user = Meteor.users.findOne({_id: arguments[0]});
-		return user && user.profile ?
-		user.profile.firstname + ' ' + user.profile.lastname :
-			'-';
-	},
-	comment: function () {
-		return this.comment !== '' ? this.comment : '-';
-	},
-	isHasMoreItems: function () {
-		let page = Template.instance().page.get();
-		return LeaveRequests.find().count() / (page * this.itemPerPage) >= 1;
-	}
+  leaveRequestsItems: function () {
+    return LeaveRequests.find();
+  },
+  date: function () {
+    return arguments[0].toLocaleDateString();
+  },
+  username: function () {
+    var user = Meteor.users.findOne({_id: arguments[0]});
+    return user && user.profile ?
+    user.profile.firstname + ' ' + user.profile.lastname :
+      '-';
+  },
+  comment: function () {
+    return this.comment !== '' ? this.comment : '-';
+  },
+  isHasMoreItems: function () {
+    let page = Template.instance().page.get();
+    return LeaveRequests.find().count() / (page * this.itemPerPage) >= 1;
+  }
 });
 Template.leaveRequestsList.events({
-	'click [data-action="load-more"]': (e, t) => {
-		e.preventDefault();
-		var page = t.page.get();
-		t.page.set(page + 1);
-	}
+  'click [data-action="load-more"]': (e, t) => {
+    e.preventDefault();
+    var page = t.page.get();
+    t.page.set(page + 1);
+  }
 });
