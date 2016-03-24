ca1ea9082b5b14da2205c190db2d2ffaa70363d3
Taras Dolynyuk <trsdln@gmail.com>
2016-03-24 15:19:28 +0200

    Ordering small refactor + fix order note

--- client/views/app/stocktake/stocktake-ordering/stocktake-ordering.js
+++ client/views/app/stocktake/stocktake-ordering/stocktake-ordering.js
@@ -16,44 +16,11 @@ Template.stocktakeOrdering.helpers({
     return !!Orders.findOne({stocktakeId: this.stocktakeId});
   },
 
-  orderItems: function (activeOrder) {
-    return OrderItems.find({orderId: activeOrder._id});
-  },
-
-  orderNote: function (activeSupplierId) {
-    var currentOrder = Orders.findOne({
-      supplierId: activeSupplierId
-    });
-
-    return currentOrder && currentOrder.orderNote;
-  },
-
   onSupplierChanged: function () {
     var tmpl = Template.instance();
     return function (supplierId) {
       tmpl.activeSupplierId.set(supplierId);
     };
-  },
-
-  isOrderEditable: function (order) {
-    return !order.orderedThrough;
   }
 });
 
-Template.stocktakeOrdering.events({
-  'keyup .supplier-order-note': function (event, tmpl) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      let noteText = tmpl.$(event.target).val().trim();
-
-      if (noteText.length > 0) {
-        let activeSupplierId = tmpl.activeSupplierId.get();
-        let currentOrder = Orders.findOne({supplierId: activeSupplierId});
-
-        currentOrder.orderNote = noteText;
-
-        Meteor.call('updateOrder', currentOrder, HospoHero.handleMethodResult());
-      }
-    }
-  }
-});
