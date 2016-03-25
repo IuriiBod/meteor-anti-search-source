00de9cd6002963bf0abcbc744db762839f423cab
vova tymets <volodymyrtymets@gmail.com>
2016-03-25 10:47:18 +0200

    show shared link if has position

--- client/views/app/applications/form-settings/form-settings.js
+++ client/views/app/applications/form-settings/form-settings.js
@@ -2,5 +2,8 @@ Template.applicationFormSettings.helpers({
   applicationId() {
     let applicationDefinition = ApplicationDefinitions.findOne();
     return applicationDefinition && applicationDefinition._id;
+  },
+  isHasPositions(){
+    return Positions.findOne() ?  true : false;
   }
 });
