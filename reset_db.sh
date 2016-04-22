5948cac920c49b3b02628a1adf4151b84bd6b4b1
Taras Dolynyuk <trsdln@gmail.com>
2016-04-22 11:39:00 +0300

    Improve rest db script

--- reset_db.sh
+++ reset_db.sh
@@ -1,30 +1,86 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #
-# Run this script with "d" argument if you want to refresh dump before restore
+# Arguments
+# -d use previously created dump
+# -s <server_type>
+# -p prevent password reset for admin
 #
 
 DEST_DIR="./.dump"
 DB_PATH=$(pwd)/.meteor/local/db
 DB_WAIT_TIME=10
 
+LOCAL_DB_PORT=3000
+LOCAL_DB_HOST=127.0.0.1
+LOCAL_DB_NAME=meteor
+
+# PASSWORD: qweqweqwe
+read -d '' RESET_ADMIN_SCRIPT <<"JS_SCRIPT"
+var meteorDb = db.getSiblingDB('meteor');
+meteorDb.users.update({'emails.address': 'tom@hospohero.com'}, {$set: {'services.password.bcrypt': '$2a$10$q3hxdkZXLsRCQZcj00oOa.8NLSkXDw958G3zkwGPzp/v.Ezphv0T2'}});
+JS_SCRIPT
+
 #credentials
 
-#testing
-#mongodb://heroku_hs84b9x2:2roa8aitcd29rlm9fkukvs6b2s@ds013041-a0.mlab.com:13041,ds013041-a1.mlab.com:13041/heroku_hs84b9x2?replicaSet=rs-ds013041
-MONGO_USER="heroku_hs84b9x2"
-MONGO_PASSWORD="2roa8aitcd29rlm9fkukvs6b2s"
-MONGO_DOMAIN="ds013041-a0.mlab.com:13041"
-MONGO_DB="heroku_hs84b9x2"
+testingDbCredetials() {
+  echo "Selected TESTING DB";
+  MONGO_USER="heroku_hs84b9x2"
+  MONGO_PASSWORD="2roa8aitcd29rlm9fkukvs6b2s"
+  MONGO_DOMAIN="ds013041-a0.mlab.com:13041"
+  MONGO_DB="heroku_hs84b9x2"
+}
 
-#production
-#MONGO_USER="cloudninja"
-#MONGO_PASSWORD="Bertaroo1"
-#MONGO_DOMAIN="c144.capital.3.mongolayer.com:10144"
-#MONGO_DB="herochef"
+productionDbCredentials() {
+  echo "Selected PRODUCTION DB";
+  MONGO_USER="cloudninja"
+  MONGO_PASSWORD="Bertaroo1"
+  MONGO_DOMAIN="c144.capital.3.mongolayer.com:10144"
+  MONGO_DB="herochef"
+}
 
 
-if [[ $1 != "d" ]]; then
+#parse script arguments
+while [[ "$#" -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -p|--prevent-password-reset)
+    echo "Prevent password reset: YES"
+    PREVENT_PASSWORD_RESET="YES"
+    ;;
+    -d|--dump)
+    echo "Use dump: YES"
+    USE_DUMP="YES"
+    ;;
+    -s|--server)
+    SERVER_TYPE="$2"
+    shift # past value argument
+    ;;
+    *)
+      echo "Unknown option ${1}"      # unknown option
+      exit 1
+    ;;
+  esac
+
+  shift # past argument or value
+done
+
+# set up server credentials
+case "${SERVER_TYPE}" in
+  "testing")
+    testingDbCredetials
+    ;;
+  "production")
+    productionDbCredentials
+    ;;
+  *)
+    testingDbCredetials
+    ;;
+esac
+
+
+if [[ "${USE_DUMP}" != "YES" ]]; then
   # refresh dump
   echo "Making remote database dump..."
   rm -rf "${DEST_DIR}"
@@ -36,9 +92,15 @@ rm -rf .meteor/local/db
 mkdir .meteor/local .meteor/local/db
 
 echo "Starting local database ..."
-mongod --dbpath="${DB_PATH}" --port=3000 --storageEngine=mmapv1 --nojournal > /dev/null & sleep ${DB_WAIT_TIME}
+mongod --dbpath="${DB_PATH}" --port="${LOCAL_DB_PORT}" --storageEngine=mmapv1 --nojournal > /dev/null & sleep ${DB_WAIT_TIME}
 
-mongorestore --host=127.0.0.1 --port=3000 --db=meteor --drop  "${DEST_DIR}/${MONGO_DB}"
+mongorestore --host=${LOCAL_DB_HOST} --port=${LOCAL_DB_PORT} --db=${LOCAL_DB_NAME} --drop  "${DEST_DIR}/${MONGO_DB}"
+
+#reset admin password
+if [[ PREVENT_PASSWORD_RESET != "YES" ]]; then
+  echo "Reset admin password ..."
+  mongo  --host=${LOCAL_DB_HOST} --port=${LOCAL_DB_PORT} --eval "${RESET_ADMIN_SCRIPT}"
+fi
 
 kill $! #kill db server
 
