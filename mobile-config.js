2b78e3def25f6ddf176d8945661594acea17e6fd
Taras Dolynyuk <trsdln@gmail.com>
2016-04-26 17:46:04 +0300

    Increase mobile app version

--- mobile-config.js
+++ mobile-config.js
@@ -2,7 +2,7 @@ App.info({
   id: 'com.tomhay.hospohero',
   name: 'Hospo Hero',
   description: 'Taking a lot of the kitchen manual tasks and making them quick, easy and accurate',
-  version: '0.1.4',
+  version: '0.1.6',
   author: 'Tom Hay',
   email: 'info@hospohero.com',
   website: 'https://app.hospohero.com'
